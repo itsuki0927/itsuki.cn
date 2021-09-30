a1992582577d886f285372fad5e47ce6a51b3ad3
itsuki0927 <2309899048@qq.com>
2021-10-01 07:48:56 +0800

    feat: add footer layout

---
+++ components/Layout/Footer/index.tsx
@@ -0,0 +1,14 @@
+import styles from './style.module.scss';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <span>Made with</span>
+      <span>❤️</span>
+      <span>by</span>
+      Itsuki
+    </footer>
+  );
+};
+
+export default Footer;

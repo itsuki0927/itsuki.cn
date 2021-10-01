ac2588ee9f03c1458abd73b866140b7dceb5e659
itsuki0927 <2309899048@qq.com>
2021-10-01 19:45:13 +0800

    feat: add path variables

---
+++ utils/request.ts
@@ -0,0 +1,15 @@
+import { WrapperResponse } from '@/entities/http';
+import axios, { AxiosResponse } from 'axios';
+
+const baseURL = `${process.env.NEXT_PUBLIC_API_BASE_PATH}/api/${process.env.NEXT_PUBLIC_API_VERSION}`;
+
+const request = axios.create({
+  baseURL,
+  timeout: 10000,
+});
+
+request.interceptors.response.use((res: AxiosResponse<WrapperResponse>) => {
+  return res.data;
+});
+
+export default request;

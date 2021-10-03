acaad43a68ff277136e18fc625d1c75839e2c112
itsuki0927 <2309899048@qq.com>
2021-10-03 08:06:17 +0800

    feat: add error page

---
+++ pages/_error.tsx
@@ -0,0 +1,13 @@
+import Error from 'next/error';
+
+const ErrorPage = ({ statusCode }: any) => {
+  console.log(`%c [statusCode: ${statusCode}] `, 'background: #fafafa; color: #ff4d4f');
+  return <Error statusCode={statusCode} />;
+};
+
+ErrorPage.getInitialProps = ({ res, err }: any) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  return { statusCode };
+};
+
+export default ErrorPage;

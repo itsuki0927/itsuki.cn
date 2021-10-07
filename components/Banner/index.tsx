7cd25b01e7aed8b1892b3141db3c95f6dc4165f9
itsuki0927 <2309899048@qq.com>
2021-10-07 09:38:34 +0800

    fix: fix banner file name

---
+++ components/Banner/index.tsx
@@ -0,0 +1,56 @@
+import classNames from 'classnames';
+import { useMemo } from 'react';
+import Card from '../Card';
+import styles from './style.module.scss';
+import { getExpandsValue } from './util';
+
+type BannerDataProps = {
+  name: string;
+  description: string;
+  path: string;
+  sort: number;
+  expand?: string;
+};
+type BannerProps = {
+  data?: BannerDataProps;
+};
+
+const Banner = ({ data }: BannerProps) => {
+  console.log(data);
+
+  const expands = useMemo(() => {
+    if (data?.expand) {
+      return JSON.parse(data.expand);
+    }
+    return null;
+  }, [data?.expand]);
+
+  if (!data) return <Card>loading...</Card>;
+
+  const renderIcon = () => {
+    const value = getExpandsValue(expands, 'icon');
+
+    if (value === null) return null;
+
+    return <i className={classNames('iconfont', `icon-${value}`, styles.logo)}></i>;
+  };
+
+  const currentBackgroundImage = getExpandsValue(expands, 'background');
+
+  return (
+    <Card className={styles.banner}>
+      <div
+        style={{
+          backgroundImage: `url(${currentBackgroundImage})`,
+        }}
+        className={styles.background}
+      ></div>
+      <div className={styles.content}>
+        {renderIcon()}
+        <p className={styles.description}>{data?.description}</p>
+      </div>
+    </Card>
+  );
+};
+
+export default Banner;

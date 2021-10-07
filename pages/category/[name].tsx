44ef5520ba5217cf0e416faa7cee7382ef7d2576
itsuki0927 <2309899048@qq.com>
2021-10-07 09:36:23 +0800

    feat: add category [name] page

--- pages/category/[name].tsx
+++ pages/category/[name].tsx
@@ -1,37 +1,27 @@
-import { getArticles } from '@/api/article';
-import ArticleList from '@/components/Article/List';
-import Card from '@/components/Card';
-import Banner from '@/components/Pages/Category/Banner';
-import { Article } from '@/entities/article';
-import { SearchResponse } from '@/entities/response/base';
+import ArticleWrapper from '@/components/Article';
+import Banner from '@/components/Banner';
 import AppContext from '@/utils/context';
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
 import { useContext, useMemo } from 'react';
 
 type StaticProps = {
-  articles: SearchResponse<Article>;
   name: string;
 };
 export const getServerSideProps: GetServerSideProps<StaticProps> = async ({ params }) => {
-  const articles = await getArticles({ category: 2, pageSize: 2 });
   return {
     props: {
-      articles,
       name: params?.name as string,
     },
   };
 };
 
-const Category = ({ articles, name }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+const Category = ({ name }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const context = useContext(AppContext);
   const category = useMemo(() => {
     console.log('context', context);
     return context?.categories?.find(item => item.path === name);
   }, [name, context]);
-
-  console.log('category', category);
 
   return (
     <div>
@@ -39,9 +29,9 @@ const Category = ({ articles, name }: InferGetServerSidePropsType<typeof getServ
         <link rel='stylesheet' href='//at.alicdn.com/t/font_2836612_bmgp99va00d.css' />
       </Head>
 
-      <Banner category={category} />
+      <Banner data={category} />
 
-      <ArticleList articles={articles} />
+      <ArticleWrapper query={{ category: category?.id, pageSize: 2 }} />
     </div>
   );
 };

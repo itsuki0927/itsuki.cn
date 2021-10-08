dd295c945bb8061a72efb49688f1ea903d62b568
itsuki0927 <2309899048@qq.com>
2021-10-08 16:32:14 +0800

    feat: error page add type declare

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,12 +1,17 @@
 /* eslint-disable no-console */
+import { NextPage } from 'next';
 import Error from 'next/error';
 
-const ErrorPage = ({ statusCode }: any) => {
-  console.log(`%c [statusCode: ${statusCode}] `, 'background: #fafafa; color: #ff4d4f');
-  return <Error statusCode={statusCode} />;
+type ErrorPageProps = {
+  statusCode?: number;
 };
 
-ErrorPage.getInitialProps = ({ res, err }: any) => {
+const ErrorPage: NextPage<ErrorPageProps> = ({ statusCode }) => {
+  console.log(`%c [statusCode: ${statusCode}] `, 'background: #fafafa; color: #ff4d4f');
+  return <Error statusCode={statusCode || 500} />;
+};
+
+ErrorPage.getInitialProps = ({ res, err }) => {
   // eslint-disable-next-line no-nested-ternary
   const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
   return { statusCode };

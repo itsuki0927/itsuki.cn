595c52f0fa9b50a1d7dc030b47403d7d9e482399
itsuki0927 <2309899048@qq.com>
2021-10-12 13:56:38 +0800

    feat(util): add mergeRef util

---
+++ utils/mergeRef.ts
@@ -0,0 +1,22 @@
+type CallbackRef<T> = (ref: T | null) => void;
+type Ref<T> = React.MutableRefObject<T> | CallbackRef<T>;
+
+function toFnRef<T>(ref?: Ref<T> | null) {
+  return !ref || typeof ref === 'function'
+    ? ref
+    : (value: T) => {
+        // eslint-disable-next-line no-param-reassign
+        ref.current = value;
+      };
+}
+
+function mergeRefs<T>(refA?: Ref<T> | null, refB?: Ref<T> | null) {
+  const a = toFnRef(refA);
+  const b = toFnRef(refB);
+  return (value: T | null) => {
+    if (typeof a === 'function') a(value!);
+    if (typeof b === 'function') b(value!);
+  };
+}
+
+export default mergeRefs;

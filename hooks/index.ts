295f1055ffd19515f4e46b25d93faf548e3c17fd
itsuki0927 <2309899048@qq.com>
2021-10-12 13:14:12 +0800

    feat(hook): add useEventListener hook

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,5 +1,6 @@
 export { default as useArticleLike } from './useArticleLike';
 export { default as useCommentLike } from './useCommentLike';
+export { default as useEventListener } from './useEventListener';
 export { default as useLocalStorage } from './useLocalStorage';
 export { default as useMount } from './useMount';
 export { default as useUnMount } from './useUnMount';
---
+++ hooks/useEventListener/index.ts
@@ -0,0 +1,28 @@
+import { useEffect } from 'react';
+import { on } from '@/utils/events';
+
+/**
+ * 给元素绑定事件
+ *
+ * @param eventTarget 元素
+ * @param event 事件名
+ * @param listener 处理事件
+ * @param options 其他参数
+ */
+function useEventListener<K extends keyof DocumentEventMap>(
+  eventTarget: EventTarget | (() => EventTarget),
+  event: K,
+  listener: EventListenerOrEventListenerObject,
+  options: boolean | AddEventListenerOptions = false
+) {
+  useEffect(() => {
+    const target = typeof eventTarget === 'function' ? eventTarget() : eventTarget;
+    const controller = target ? on(target, event, listener, options) : null;
+
+    return () => {
+      controller?.off();
+    };
+  }, [event, eventTarget, listener, options]);
+}
+
+export default useEventListener;

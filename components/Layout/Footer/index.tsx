b1d23db1f215ba17e8042cb2fc9de58a07b72dde
itsuki0927 <2309899048@qq.com>
2021-10-18 13:24:36 +0800

    chore(layout): optimize footer style

--- components/Layout/Footer/index.tsx
+++ components/Layout/Footer/index.tsx
@@ -1,11 +1,16 @@
+import Card from '@/components/Card';
 import styles from './style.module.scss';
 
 const Footer = () => (
   <footer className={styles.footer}>
-    <span>Made with</span>
-    <span>❤️</span>
-    <span>by</span>
-    Itsuki
+    <Card bordered={false} bodyStyle={{ padding: '16px' }}>
+      <div className={styles.container}>
+        <span>Made with</span>
+        <span>❤️</span>
+        <span>by</span>
+        Itsuki
+      </div>
+    </Card>
   </footer>
 );
 

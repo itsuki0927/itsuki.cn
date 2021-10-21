da28da6e5dd8d98c1e90468a04bbdd7b799b0f8a
itsuki0927 <2309899048@qq.com>
2021-10-21 19:52:14 +0800

    feat(component): add page loading progress component

---
+++ components/PageLoadingProgress/index.tsx
@@ -0,0 +1,41 @@
+import classNames from 'classnames';
+import { useRouter } from 'next/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+import { useEffect, useState } from 'react';
+import styles from './style.module.scss';
+
+const PageLoadingProgress = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  const handleStart = (url: string) => {
+    // eslint-disable-next-line no-console
+    console.log(`[Loading: ${url}]`);
+    setIsLoading(true);
+    NProgress.start();
+  };
+
+  const handleStop = () => {
+    // eslint-disable-next-line no-console
+    console.log('[Loaded]');
+    setIsLoading(false);
+    NProgress.done();
+  };
+
+  useEffect(() => {
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleStop);
+    router.events.on('routeChangeError', handleStop);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleStop);
+      router.events.off('routeChangeError', handleStop);
+    };
+  }, [router.events]);
+
+  return <div className={classNames(styles.overlay, { [styles.show]: isLoading })} />;
+};
+
+export default PageLoadingProgress;

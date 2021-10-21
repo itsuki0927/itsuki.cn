2aa717589b7e77aa24e47c12076579c936b52ece
itsuki0927 <2309899048@qq.com>
2021-10-21 19:57:56 +0800

    chore: home slider state -> props

--- components/Page/home/Slider.tsx
+++ components/Page/home/Slider.tsx
@@ -1,69 +1,55 @@
 import dynamic from 'next/dynamic';
 import Image from 'next/image';
 import router from 'next/router';
-import React, { useEffect, useState } from 'react';
-import { getArticles } from '@/api/article';
-import { Article } from '@/entities/article';
-import { SearchResponse } from '@/entities/response/base';
+import React from 'react';
 import imageTransformer from '@/transformers/image';
+import { SearchResponse } from '@/entities/response/base';
+import { Article } from '@/entities/article';
 import 'slick-carousel/slick/slick.css';
 import styles from './style.module.scss';
 
 const Slider = dynamic(() => import('react-slick'));
 const Card = dynamic(() => import('@/components/Card'));
 
-const HomeSlider = () => {
-  const [articles, setArticles] = useState<SearchResponse<Article>>();
-
-  useEffect(() => {
-    let isCancel = false;
-    getArticles({ publish: 1, banner: 1 }).then(res => {
-      if (!isCancel) {
-        setArticles(res);
-      }
-    });
-
-    return () => {
-      isCancel = true;
-    };
-  }, []);
-
-  return (
-    <Slider
-      accessibility
-      arrows={false}
-      autoplay
-      infinite
-      autoplaySpeed={5000}
-      className={styles.swiper}
-    >
-      {articles?.data.map(article => (
-        <Card
-          onClick={() => router.push(`/article/${article.id}`)}
-          className={styles.article}
-          cover={
-            <Image
-              src={article.cover}
-              objectFit='cover'
-              placeholder='empty'
-              width='100%'
-              height={300}
-              loader={imageTransformer}
-              alt='banner-cover'
-            />
-          }
-          key={article.id}
-        >
-          <div className={styles.text}>
-            <span className={styles.title}>
-              <strong>{article.title}</strong>
-            </span>
-            <span className={styles.description}>{article.description}</span>
-          </div>
-        </Card>
-      ))}
-    </Slider>
-  );
+type HomeSliderProps = {
+  articles?: SearchResponse<Article>;
 };
+
+const HomeSlider = ({ articles }: HomeSliderProps) => (
+  <Slider
+    accessibility
+    arrows={false}
+    autoplay
+    infinite
+    autoplaySpeed={5000}
+    className={styles.swiper}
+  >
+    {articles?.data.map(article => (
+      <Card
+        onClick={() => router.push(`/article/${article.id}`)}
+        className={styles.article}
+        cover={
+          <Image
+            src={article.cover}
+            objectFit='cover'
+            placeholder='empty'
+            width='100%'
+            height={300}
+            loader={imageTransformer}
+            alt='banner-cover'
+          />
+        }
+        key={article.id}
+      >
+        <div className={styles.text}>
+          <span className={styles.title}>
+            <strong>{article.title}</strong>
+          </span>
+          <span className={styles.description}>{article.description}</span>
+        </div>
+      </Card>
+    ))}
+  </Slider>
+);
 
 export default HomeSlider;

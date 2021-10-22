6e6d9de42dce90f2390411f9b3cba19ea7068a46
itsuki0927 <2309899048@qq.com>
2021-10-22 15:04:22 +0800

    feat: add error type declare

---
+++ helpers/utils/errors.ts
@@ -0,0 +1,42 @@
+/* eslint-disable max-classes-per-file */
+export type ErrorData = {
+  message: string;
+  code?: string;
+};
+
+export type ErrorProps = { code?: string } & (
+  | { message: string; errors?: never }
+  | { message?: never; errors: ErrorData[] }
+);
+
+export class BlogError extends Error {
+  code?: string;
+
+  errors: ErrorData[];
+
+  constructor({ message, code, errors }: ErrorProps) {
+    const error: ErrorData = message
+      ? { message, ...(code ? { code } : {}) }
+      : errors![0];
+
+    super(error.message);
+    this.errors = message ? [error] : errors!;
+    if (error.code) this.code = error.code;
+  }
+}
+
+export class ValidationError extends BlogError {
+  constructor(options: ErrorProps) {
+    super(options);
+    this.code = 'valdation_error';
+  }
+}
+
+export class FetcherError extends BlogError {
+  status: number;
+
+  constructor(options: { status: number } & ErrorProps) {
+    super(options);
+    this.status = options.status;
+  }
+}

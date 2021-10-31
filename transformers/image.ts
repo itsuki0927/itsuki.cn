c675dc1ca7b75dd979668ec45795b3f31477a6d6
itsuki0927 <2309899048@qq.com>
2021-10-31 20:54:33 +0800

    fix: fix image relative、absolute path show error

--- transformers/image.ts
+++ transformers/image.ts
@@ -1,7 +1,23 @@
 import { ImageLoaderProps } from 'next/image';
 import { RESOURCE_URL } from '@/configs/app';
 
-const imageTransformer = ({ src, width, quality }: ImageLoaderProps) =>
-  `${RESOURCE_URL}/${src}?width=${width}&quality=${quality || 70}`;
+const schemaList = ['https', 'http'];
+
+type ImageParams = Pick<ImageLoaderProps, 'width' | 'quality'>;
+
+const getImageParams = ({ width, quality }: ImageParams) =>
+  `width=${width}&quality=${quality || 70}`;
+
+/**
+ * src如果以http、https开头则直接使用src, 否则拼接生成最终的url
+ */
+const imageTransformer = ({ src, width, quality }: ImageLoaderProps) => {
+  const path = `${src}?${getImageParams({ width, quality })}`;
+  const origin = schemaList.some(prefix => src.startsWith(prefix))
+    ? ''
+    : `${RESOURCE_URL}/`;
+  const url = `${origin}${path}`;
+  return url;
+};
 
 export default imageTransformer;

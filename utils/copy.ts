e9f14fa2549d9120750d0f877d948835f2557b38
itsuki0927 <2309899048@qq.com>
2021-11-11 13:52:56 +0800

    feat(util): add copyToClipboard util

---
+++ utils/copy.ts
@@ -0,0 +1,48 @@
+/* eslint-disable no-console */
+
+/**
+ * 回滚方式
+ *
+ * @param text 复制的文本
+ */
+const fallbackCopyTextToClipboard = (text: string) => {
+  const textarea = document.createElement('textarea');
+  textarea.value = text;
+  textarea.setAttribute('readonly', '');
+  textarea.style.position = 'absolute';
+  textarea.style.left = '-99999px';
+  document.body.appendChild(textarea);
+
+  const rangeCount = document.getSelection()?.rangeCount || 0;
+  const select = rangeCount > 0 ? document.getSelection()?.getRangeAt(0) : null;
+  textarea.select();
+  document.execCommand('copy');
+
+  document.body.removeChild(textarea);
+  if (select) {
+    document.getSelection()?.removeAllRanges();
+    document.getSelection()?.addRange(select);
+  }
+};
+
+/**
+ * 剪切板复制
+ *
+ * @param text 需要复制的内容
+ */
+const copyTextToClipboard = (text: string) => {
+  if (!navigator.clipboard) {
+    fallbackCopyTextToClipboard(text);
+    return;
+  }
+  navigator.clipboard.writeText(text).then(
+    () => {
+      console.log('[ Async: Copying to clipboard was successful! ]');
+    },
+    err => {
+      console.error('Async: Could not copy text: ', err);
+    }
+  );
+};
+
+export default copyTextToClipboard;
---
+++ utils/index.ts
@@ -0,0 +1,1 @@
+export { default as copyTextToClipboard } from './copy';

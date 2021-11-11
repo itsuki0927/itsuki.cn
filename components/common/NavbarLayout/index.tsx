2e803c0a1371fe14131a1968c4d350803c30b261
itsuki0927 <2309899048@qq.com>
2021-11-11 20:23:00 +0800

    fix(layout): fix NavbarLayout no blog context

--- components/common/NavbarLayout/index.tsx
+++ components/common/NavbarLayout/index.tsx
@@ -1,14 +1,21 @@
 import classNames from 'classnames';
 import React, { FC } from 'react';
+import { useRouter } from 'next/router';
+import { BlogProvider } from '@/framework/local';
 import { Navbar } from '..';
 import { PageProps } from '../Layout';
 import styles from './style.module.scss';
 
-const NavbarLayout: FC<PageProps> = ({ pageProps, children }) => (
-  <div className={styles.navbarLayout}>
-    <Navbar search={false} links={pageProps.categories} />
-    <main className={classNames(styles.main, 'container')}>{children}</main>
-  </div>
-);
+const NavbarLayout: FC<PageProps> = ({ pageProps, children }) => {
+  const { locale = 'zh-cn' } = useRouter();
+  return (
+    <BlogProvider locale={locale}>
+      <div className={styles.navbarLayout}>
+        <Navbar search={false} links={pageProps.categories} />
+        <main className={classNames(styles.main, 'container')}>{children}</main>
+      </div>
+    </BlogProvider>
+  );
+};
 
 export default NavbarLayout;

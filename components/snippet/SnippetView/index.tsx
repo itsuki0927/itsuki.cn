8bd6b8f2c21d8635906f96f9d8683c2ef1c59dd4
itsuki0927 <2309899048@qq.com>
2021-11-11 14:41:45 +0800

    feat(page): add snippet detail ranks status

--- components/snippet/SnippetView/index.tsx
+++ components/snippet/SnippetView/index.tsx
@@ -1,9 +1,16 @@
 import Link from 'next/link';
+import classNames from 'classnames';
 import { JsTextOutlined } from '@/components/icons';
 import { Card } from '@/components/ui';
 import { SearchResponse } from '@/entities/response/base';
 import { Snippet } from '@/entities/snippet';
 import styles from './style.module.scss';
+
+export enum RanksState {
+  Easy = 0, // 简单
+  Medium = 1, // 中等
+  Hard = 2, // 困难
+}
 
 interface SnippetViewProps {
   snippets: SearchResponse<Snippet>;
@@ -17,7 +24,13 @@ const SnippetView = ({ snippets }: SnippetViewProps) => (
           avatar={
             <span className={styles.icon}>
               <JsTextOutlined style={{ fontSize: 26, lineHeight: '48px' }} />
-              <i className={styles.expertise} />
+              <i
+                className={classNames(styles.expertise, {
+                  [styles.easy]: snippet.ranks === RanksState.Easy,
+                  [styles.medium]: snippet.ranks === RanksState.Medium,
+                  [styles.hard]: snippet.ranks === RanksState.Hard,
+                })}
+              />
             </span>
           }
           title={

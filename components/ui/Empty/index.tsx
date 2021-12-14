b041b5fd76883a414537362048c5a1eef833c8cb
itsuki0927 <2309899048@qq.com>
2021-12-14 21:23:53 +0800

    chore(component): optimize Empty style

--- components/ui/Empty/index.tsx
+++ components/ui/Empty/index.tsx
@@ -1,11 +1,11 @@
-import { Icon } from '@/components/icons';
+import { EmptyOutlined } from '@/components/icons';
 import { Card } from '@/components/ui';
 import styles from './style.module.scss';
 
 const Empty = () => (
   <Card className={styles.empty} bodyStyle={{ padding: '12px 0' }}>
-    <Icon name='empty' className={styles.icon} />
-    <p>暂无数据</p>
+    <EmptyOutlined className={styles.icon} />
+    <p className={styles.description}>暂无数据</p>
   </Card>
 );
 

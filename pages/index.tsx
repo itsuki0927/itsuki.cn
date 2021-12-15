e206c0e46e1ac8cdde757505f3c6017e8d39ef6f
itsuki0927 <2309899048@qq.com>
2021-12-15 10:19:58 +0800

    chore: home page alert add padding

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,7 +39,7 @@ const HomePage = ({
     <HomeSlider articles={bannerArticles} />
 
     <Alert
-      style={{ marginBottom: 24 }}
+      style={{ marginBottom: 24, padding: '8px 24px' }}
       message='思考比写代码来的更加珍贵'
       type='info'
       icon={<SoundOutlined />}

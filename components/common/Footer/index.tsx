2d17dad3ce7e50e2ee2634da07a03da7f4cc4950
itsuki0927 <2309899048@qq.com>
2021-12-18 20:11:42 +0800

    feat(layout): update layout style

--- components/common/Footer/index.tsx
+++ components/common/Footer/index.tsx
@@ -1,24 +1,18 @@
-import { Card } from '@/components/ui';
 import styles from './style.module.scss';
 
 const Footer = () => (
   <footer className={styles.footer}>
-    <Card bodyStyle={{ padding: '16px' }}>
-      <div className='container'>
-        <span>Made with</span>
-        <span>❤️</span>
-        <span>by</span>
-        Itsuki
-        <a
-          className={styles.link}
-          href='https://beian.miit.gov.cn'
-          target='_blank'
-          rel='noreferrer'
-        >
-          湘ICP备2021020356号-1
-        </a>
-      </div>
-    </Card>
+    <div className='container'>
+      Build by 🖐️ 🪵 🇨🇳 👨‍💻 🌈 🎈
+      <a
+        className={styles.link}
+        href='https://beian.miit.gov.cn'
+        target='_blank'
+        rel='external nofollow noopener noreferrer'
+      >
+        湘ICP备2021020356号-1
+      </a>
+    </div>
   </footer>
 );
 

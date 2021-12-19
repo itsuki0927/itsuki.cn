cc23bc2ebb3dcfe897fc2a9ecb9a39fbc2c4a252
itsuki0927 <2309899048@qq.com>
2021-12-19 15:18:02 +0800

    feat: optimize sidebar layout spacing

--- components/common/Sidebar/Tag/index.tsx
+++ components/common/Sidebar/Tag/index.tsx
@@ -1,22 +1,17 @@
 import { ArticleTag } from '@/components/article';
-import { Card } from '@/components/ui';
 import { SiteInfo } from '@/entities/siteInfo';
 import styles from './style.module.scss';
-
-const cardBodyStyle = {
-  paddingBottom: 12,
-};
 
 interface SidebarTagProps {
   tags: SiteInfo['tags'];
 }
 
 const SidebarTag = ({ tags }: SidebarTagProps) => (
-  <Card className={styles.tag} bodyStyle={cardBodyStyle}>
+  <div className={styles.tag}>
     {tags?.map(item => (
       <ArticleTag key={item.id} className={styles.btn} tag={item} />
     ))}
-  </Card>
+  </div>
 );
 
 export default SidebarTag;

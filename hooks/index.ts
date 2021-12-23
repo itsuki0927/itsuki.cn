0a5efd1ebe2c6a66e66c33c280f56598c455f973
itsuki0927 <2309899048@qq.com>
2021-12-23 14:32:16 +0800

    feat(hook): add useTimeout hook

--- hooks/index.ts
+++ hooks/index.ts
@@ -4,4 +4,5 @@ export { default as useCounter } from './useCounter';
 export { default as useEventListener } from './useEventListener';
 export { default as useLocalStorage } from './useLocalStorage';
 export { default as useMount } from './useMount';
+export { default as useTimeout } from './useTimeout';
 export { default as useUnMount } from './useUnMount';

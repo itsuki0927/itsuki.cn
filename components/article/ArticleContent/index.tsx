a77b57a6d9af118ce1cc3cd47f6bd84447f3db4c
itsuki0927 <2309899048@qq.com>
2022-01-04 19:42:08 +0800

    chore: update article content style

--- components/article/ArticleContent/index.tsx
+++ components/article/ArticleContent/index.tsx
@@ -1,4 +1,4 @@
-import { Card, MarkdownBlock } from '@/components/ui';
+import { MarkdownBlock } from '@/components/ui';
 import { ao } from '@/constants/article/origin';
 import { Article } from '@/entities/article';
 import styles from './style.module.scss';
@@ -11,17 +11,17 @@ const ArticleContent = ({ article }: ArticleContentProps) => {
   const origin = ao(article.origin);
 
   return (
-    <Card style={{ marginBottom: 24 }}>
+    <div className={styles.content}>
       <div className={styles.title}>
         <h2>{article.title}</h2>
         <span style={{ background: origin.color }} className={styles.origin}>
           {origin.icon}
-          <span style={{ marginLeft: 8 }}>{origin.name}</span>
+          <span className={styles.text}>{origin.name}</span>
         </span>
       </div>
 
       <MarkdownBlock htmlContent={article.content} />
-    </Card>
+    </div>
   );
 };
 

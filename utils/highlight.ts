744154b0a4e935423be3c28de7caa9a887544d2e
liusenlin <liusenlin@corp.netease.com>
2022-03-14 17:34:37 +0800

    chore: optimize register、alias highlight language & add lua highlight

--- utils/highlight.ts
+++ utils/highlight.ts
@@ -1,27 +1,27 @@
 import HLJSApi from 'highlight.js';
-
 import hljs from 'highlight.js/lib/core';
-import go from 'highlight.js/lib/languages/go';
-import markdown from 'highlight.js/lib/languages/markdown';
+import bash from 'highlight.js/lib/languages/bash';
 import css from 'highlight.js/lib/languages/css';
-import sql from 'highlight.js/lib/languages/sql';
+import go from 'highlight.js/lib/languages/go';
+import java from 'highlight.js/lib/languages/java';
+import javascript from 'highlight.js/lib/languages/javascript';
+import json from 'highlight.js/lib/languages/json';
+import less from 'highlight.js/lib/languages/less';
+import lua from 'highlight.js/lib/languages/lua';
+import markdown from 'highlight.js/lib/languages/markdown';
+import nginx from 'highlight.js/lib/languages/nginx';
 import php from 'highlight.js/lib/languages/php';
+import python from 'highlight.js/lib/languages/python';
+import rust from 'highlight.js/lib/languages/rust';
+import scss from 'highlight.js/lib/languages/scss';
+import shell from 'highlight.js/lib/languages/shell';
+import sql from 'highlight.js/lib/languages/sql';
+import stylus from 'highlight.js/lib/languages/stylus';
+import typescript from 'highlight.js/lib/languages/typescript';
 import xml from 'highlight.js/lib/languages/xml';
 import yaml from 'highlight.js/lib/languages/yaml';
-import json from 'highlight.js/lib/languages/json';
-import bash from 'highlight.js/lib/languages/bash';
-import less from 'highlight.js/lib/languages/less';
-import scss from 'highlight.js/lib/languages/scss';
-import rust from 'highlight.js/lib/languages/rust';
-import java from 'highlight.js/lib/languages/java';
-import shell from 'highlight.js/lib/languages/shell';
-import nginx from 'highlight.js/lib/languages/nginx';
-import stylus from 'highlight.js/lib/languages/stylus';
-import python from 'highlight.js/lib/languages/python';
-import javascript from 'highlight.js/lib/languages/javascript';
-import typescript from 'highlight.js/lib/languages/typescript';
-
 import 'highlight.js/styles/base16/onedark.css';
+
 // import 'highlight.js/styles/base16/gruvbox-dark-hard.css';
 
 const languages = {
@@ -44,7 +44,8 @@ const languages = {
   markdown,
   javascript,
   typescript,
-};
+  lua,
+} as const;
 
 const aliasLanguage = {
   js: 'javascript',
@@ -53,12 +54,12 @@ const aliasLanguage = {
   tsx: 'typescript',
 };
 
-Object.keys(languages).forEach(name =>
-  hljs.registerLanguage(name, (languages as any)[name])
-);
+Object.entries(languages).forEach(([name, language]) => {
+  hljs.registerLanguage(name, language);
+});
 
-Object.keys(aliasLanguage).forEach(alias =>
-  hljs.registerAliases(alias, { languageName: (aliasLanguage as any)[alias] })
+Object.entries(aliasLanguage).forEach(([alias, languageName]) =>
+  hljs.registerAliases(alias, { languageName })
 );
 
 export default hljs as typeof HLJSApi;

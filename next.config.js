2c4853321d4e2b47e6f7ab836e2586310856e0e6
itsuki0927 <2309899048@qq.com>
2022-04-01 08:40:03 +0800

    chore: update next config json

--- next.config.js
+++ next.config.js
@@ -5,16 +5,18 @@ const RESOURCE_URL = `https://${RESOURCE_HOST}`;
 const isProd = process.env.NODE_ENV === 'production';
 console.log(`[ mode: ${process.env.NODE_ENV} ]`);
 
+const domains = ['s.gravatar.com', RESOURCE_HOST];
+
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: isProd,
 });
 
 /** @type {import('next').NextConfig} */
 module.exports = withBundleAnalyzer({
-  // assetPrefix: isProd ? RESOURCE_URL : '',
+  assetPrefix: isProd ? RESOURCE_URL : '',
   reactStrictMode: true,
   images: {
-    domains: [RESOURCE_HOST],
+    domains: isProd ? domains.slice(-1) : domains,
   },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // config.externals.push({

3742993c0118fe677f7909aff1a6ae1225ad3540
itsuki0927 <2309899048@qq.com>
2022-04-01 08:34:05 +0800

    chore: add queryKeys constants save all query key

---
+++ constants/queryKeys.ts
@@ -0,0 +1,17 @@
+export const articleKeys = {
+  article: 'article' as const,
+  lists: () => [articleKeys.article, 'list'] as const,
+  pagination: (current: number) => [articleKeys.article, 'page', current] as const,
+  tag: (tag: string) => [articleKeys.article, 'tag', tag] as const,
+  category: (category: string) => [articleKeys.article, 'category', category] as const,
+  detail: (id: number) => [articleKeys.article, 'detail', id] as const,
+  publish: () => ['publish', ...articleKeys.lists()] as const,
+  banner: () => ['banner', ...articleKeys.lists()] as const,
+};
+
+export const commentKeys = {
+  comment: 'comment' as const,
+  lists: (articleId: number) =>
+    [articleKeys.article, articleId, commentKeys.comment, 'list'] as const,
+  like: (id: number) => [commentKeys.comment, id, 'like'] as const,
+};

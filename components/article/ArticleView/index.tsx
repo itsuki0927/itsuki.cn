820d6f0c5e29978176add75a35bca473f9ecbe62
itsuki0927 <2309899048@qq.com>
2022-04-07 08:41:43 +0800

    feat: add RelateArticles component

--- components/article/ArticleView/index.tsx
+++ components/article/ArticleView/index.tsx
@@ -1,15 +1,13 @@
 import { ArticleJsonLd, NextSeo } from 'next-seo';
-import Image from 'next/image';
 import { CommentView } from '@/components/comment';
-import { ToDate } from '@/components/common';
 import { ImagePopup } from '@/components/ui';
-import Widget from '@/components/ui/Widget';
 import { ArticleDetailResponse } from '@/entities/article';
 import { useGlobalData } from '@/hooks/globalData';
 import { CustomWindow } from '@/types/window';
 import { getArticleDetailFullUrl } from '@/utils/url';
 import ArticleContent from '../ArticleContent';
 import ArticlePagination from '../ArticlePagination';
+import RelateArticles from '../RelateArticles';
 
 declare let window: CustomWindow;
 
@@ -39,22 +37,7 @@ const ArticleView = ({ article }: ArticleViewProps) => {
 
       <ArticleContent article={article} />
 
-      <Widget>
-        <Widget.Header> 看看这些? </Widget.Header>
-        <div className='mb-2 flex justify-between'>
-          {globalData.data?.hotArticles.slice(0, 3).map(item => (
-            <div key={item.id}>
-              <Image src={item.cover} width={210} height={158} />
-              <h2 className='cursor-pointer text-sm tracking-widest text-[#2d2d2d] transition-colors duration-500 hover:text-[#777]'>
-                {item.title}
-              </h2>
-              <span className='mb-1 text-xs tracking-wider text-[#b6b6b6]'>
-                <ToDate date={item.createAt} to='YMD' />
-              </span>
-            </div>
-          ))}
-        </div>
-      </Widget>
+      <RelateArticles relateArticles={globalData?.data?.hotArticles.slice(0, 3) ?? []} />
 
       <ArticlePagination
         prevArticle={article.prevArticle}
---
+++ components/article/RelateArticles/index.tsx
@@ -0,0 +1,41 @@
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+import { ToDate } from '@/components/common';
+import { Widget } from '@/components/ui';
+import { Article } from '@/entities/article';
+
+interface RelateArticlesProps {
+  relateArticles: Article[];
+}
+
+const RelateArticles = ({ relateArticles }: RelateArticlesProps) => {
+  const router = useRouter();
+  return (
+    <Widget>
+      <Widget.Header> 看看这些? </Widget.Header>
+      <div className='mb-2 flex justify-between'>
+        {relateArticles.map(article => (
+          <div key={article.id}>
+            <Image
+              src={article.cover}
+              width={210}
+              height={158}
+              onClick={() => router.push(`/article/${article.id}`)}
+            />
+            <h2
+              className='cursor-pointer text-sm tracking-widest text-[#2d2d2d] transition-colors duration-500 hover:text-[#777]'
+              onClick={() => router.push(`/article/${article.id}`)}
+            >
+              {article.title}
+            </h2>
+            <span className='mb-1 text-xs tracking-wider text-[#b6b6b6]'>
+              <ToDate date={article.createAt} to='YMD' />
+            </span>
+          </div>
+        ))}
+      </div>
+    </Widget>
+  );
+};
+
+export default RelateArticles;

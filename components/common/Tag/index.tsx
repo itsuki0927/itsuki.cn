b3ab9f8c1e5bf732f47897d6de5e5287293333ac
itsuki0927 <2309899048@qq.com>
2022-04-23 21:44:48 +0800

    fix: fix tag click not work

--- components/common/Tag/index.tsx
+++ components/common/Tag/index.tsx
@@ -1,16 +1,23 @@
+import { useRouter } from 'next/router';
 import { Tag as ArticleTag } from '@/entities/tag';
 
 interface TagProps {
   tag: ArticleTag;
 }
 
-const Tag = ({ tag }: TagProps) => (
-  <small
-    key={tag.id}
-    className='h-max cursor-pointer rounded-sm bg-[#faebd7] px-2 py-[2px] text-xs font-light uppercase transition-colors hover:bg-[#ffd090]'
-  >
-    # {tag.name}
-  </small>
-);
+const Tag = ({ tag }: TagProps) => {
+  const router = useRouter();
+  return (
+    <small
+      tabIndex={-1}
+      role='button'
+      key={tag.id}
+      className='h-max cursor-pointer rounded-sm bg-[#faebd7] px-2 py-[2px] text-xs font-light uppercase transition-colors hover:bg-[#ffd090]'
+      onClick={() => router.push(`/tag/${tag.path}`)}
+    >
+      # {tag.name}
+    </small>
+  );
+};
 
 export default Tag;

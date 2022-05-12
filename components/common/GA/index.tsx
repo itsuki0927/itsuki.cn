4229323f5f27e4e6a2fcf96a9be4adf62098eda2
itsuki0927 <2309899048@qq.com>
2022-05-12 22:05:58 +0800

    chore: update GA load strategy

--- components/common/GA/index.tsx
+++ components/common/GA/index.tsx
@@ -32,12 +32,12 @@ const GA = () => {
   return needGA ? (
     <>
       <Script
-        strategy='afterInteractive'
+        strategy='lazyOnload'
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
       />
       <Script
         id='gtag-init'
-        strategy='afterInteractive'
+        strategy='lazyOnload'
         dangerouslySetInnerHTML={{
           __html: `
             window.dataLayer = window.dataLayer || [];

cfdb806f5ed17cc295876cc408b367b6241d7eab
itsuki0927 <2309899048@qq.com>
2022-05-16 20:10:40 +0800

    chore: improve Tag code

--- components/common/Tag/index.tsx
+++ components/common/Tag/index.tsx
@@ -7,16 +7,16 @@ interface TagProps {
 }
 
 const Tag = ({ tag }: TagProps) => (
-  <small
-    tabIndex={-1}
-    role='button'
-    key={tag.id}
-    className='h-max cursor-pointer rounded-sm bg-[#faebd7] px-2 py-[2px] text-xs font-light uppercase transition-colors hover:bg-[#ffd090] dark:bg-[#452b09] dark:hover:bg-[#764400]'
-  >
-    <Link href={getTagUrl(tag.path)}>
-      <># {tag.name}</>
-    </Link>
-  </small>
+  <Link href={getTagUrl(tag.path)}>
+    <small
+      tabIndex={-1}
+      role='button'
+      key={tag.id}
+      className='h-max cursor-pointer rounded-sm bg-[#faebd7] px-2 py-[2px] text-xs font-light uppercase transition-colors hover:bg-[#ffd090] dark:bg-[#452b09] dark:hover:bg-[#764400]'
+    >
+      # {tag.name}
+    </small>
+  </Link>
 );
 
 export default Tag;

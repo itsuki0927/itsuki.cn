66c92ed0fc05ced56665980d23b5e9cd0bef6268
itsuki0927 <2309899048@qq.com>
2022-05-27 20:10:21 +0800

    chore: remove unnecessary ui components

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -6,8 +6,6 @@ export { default as Carousel } from './Carousel';
 export { default as Container } from './Container';
 export { default as Empty } from './Empty';
 export { default as Popup } from './Popup';
-export { default as Input } from './Input';
-export { default as Loading } from './Loading';
 export { default as MarkdownBlock } from './MarkdownBlock';
 export { default as PageLoadingProgress } from './PageLoadingProgress';
 export { default as Widget } from './Widget';

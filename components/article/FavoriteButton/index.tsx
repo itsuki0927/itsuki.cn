41193a422407b1791c29f01f5470e22ad2f91956
itsuki0927 <2309899048@qq.com>
2022-05-28 14:30:56 +0800

    chore: update FavoriteButton num font-weight

--- components/article/FavoriteButton/index.tsx
+++ components/article/FavoriteButton/index.tsx
@@ -18,7 +18,7 @@ const FavoriteButton = ({ isLike, liking, onLike }: FavoriteButtonProps) => {
     <button
       type='button'
       className={classNames(
-        'flex items-center rounded-sm bg-danger px-6 py-2 text-sm font-medium text-white outline-none',
+        'flex items-center space-x-3 rounded-sm bg-danger px-6 py-2 text-sm font-medium text-white outline-none',
         isLike
           ? 'cursor-not-allowed'
           : 'transition-colors duration-300 hover:bg-danger-hover'
@@ -26,7 +26,7 @@ const FavoriteButton = ({ isLike, liking, onLike }: FavoriteButtonProps) => {
       onClick={handleLike}
     >
       {isLike ? <LikeFilled className='text-white' /> : <LikeOutlined />}
-      <strong className='capsize ml-2 mr-1 text-white'>{liking}</strong>
+      <strong className='capsize font-medium text-white'>{liking}</strong>
     </button>
   );
 };

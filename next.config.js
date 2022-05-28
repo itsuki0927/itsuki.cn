fbcae24fe237d84d190d227d9604574b56c13649
itsuki0927 <2309899048@qq.com>
2022-05-28 09:46:02 +0800

    chore: clean next.config.js

--- next.config.js
+++ next.config.js
@@ -5,8 +5,6 @@ const RESOURCE_HOST = 'static.itsuki.cn';
 const isProd = process.env.NODE_ENV === 'production';
 console.log(`[ mode: ${process.env.NODE_ENV} ]`);
 
-const domains = ['gravatar.loli.net', RESOURCE_HOST];
-
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: isProd,
 });
@@ -20,62 +18,13 @@ module.exports = withBundleAnalyzer({
     return [{ source: '/rss.xml', destination: '/api/rss' }];
   },
   images: {
-    domains,
+    domains: [RESOURCE_HOST],
   },
   compiler: {
     // 生产环境清除console.log
     removeConsole: isProd ? { exclude: ['error'] } : false,
   },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Find the base rule that contains nested rules (which contains css-loader)
-    const rules = config.module.rules.find(r => !!r.oneOf);
-
-    // Interate over the found rules
-    rules.oneOf.forEach(loaders => {
-      // Focus on the the loaders that have an array of `use` statements
-      if (Array.isArray(loaders.use)) {
-        // Iterate over each of the loaders
-        loaders.use.forEach(l => {
-          // Only focus on loaders that are an object and have a `loader` property set to `css-loader`
-          if (
-            typeof l !== 'string' &&
-            typeof l.loader === 'string' &&
-            /(?<!post)css-loader/.test(l.loader)
-          ) {
-            // If there are no module options originally set, skip this loader
-            if (!l.options.modules) return;
-            const { getLocalIdent, ...others } = l.options.modules;
-
-            // Create a new options object with the `getLocalIdent` property set to a function
-            l.options = {
-              ...l.options,
-              modules: {
-                ...others,
-                getLocalIdent: (ctx, localIdentName, localName) => {
-                  // If the class name is `dark`, return it instead of hashing it
-                  if (localName === 'dark') return localName;
-                  // Otherwise, call the original function and return the value
-                  return getLocalIdent(ctx, localIdentName, localName);
-                },
-              },
-            };
-          }
-        });
-      }
-    });
-
-    // config.externals.push({
-    //   'hightlight.js': 'hightlight.js',
-    //   marked: 'marked',
-    //   dompurify: 'dompurify',
-    // });
-
-    // config.performance = {
-    //   hints: 'error',
-    //   maxAssetSize: 500 * 1024, // 100 KiB
-    //   maxEntrypointSize: 500 * 1024, // 100 KiB
-    // };
-
     config.optimization = {
       ...config.optimization,
       splitChunks: {

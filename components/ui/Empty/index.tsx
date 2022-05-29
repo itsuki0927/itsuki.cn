875d2429c0c0799b5085e73c69e0fd8ffd67a2c8
itsuki0927 <2309899048@qq.com>
2022-05-29 08:15:47 +0800

    style: update Empty style

--- components/ui/Empty/index.tsx
+++ components/ui/Empty/index.tsx
@@ -1,18 +1,11 @@
-import { Icon } from '@/components/icons';
 import Container from '../Container';
 
 interface EmptyProps {
-  icon?: string;
+  description?: string;
 }
 
-const Empty = ({ icon = 'empty' }: EmptyProps) => (
-  <Container className='text-center'>
-    <Icon name={icon} className='text-4xl text-gray-1 ' />
-    <p className='mt-1 text-sm tracking-widest text-gray-3 '>
-      不找了, 找不到了, 这世界已经疯了
-      <span className='absolute mt-1 text-xs text-gray-1 '>《不找了》</span>
-    </p>
-  </Container>
+const Empty = ({ description = '空空如也' }: EmptyProps) => (
+  <Container className='p-8 text-center text-lg text-gray-3'>{description}</Container>
 );
 
 export default Empty;

5d5674338e6147dda4f4112cd6a06b0af1dc3b0e
itsuki0927 <2309899048@qq.com>
2022-05-29 10:28:19 +0800

    chore: improve marked

--- libs/marked.ts
+++ libs/marked.ts
@@ -1,4 +1,5 @@
 import { marked } from 'marked';
+import { WEB_URL } from '@/configs/app';
 import highlight from './highlight';
 import purifyDomString from './purify';
 
@@ -16,99 +17,116 @@ marked.use({
   },
 });
 
-const customRenderer = new marked.Renderer();
+interface RendererGetterOption {
+  purify: boolean;
+  text: (text: string) => string;
+  headingID: (html: string, level: number, raw: string) => string;
+}
 
-// 解析段落
-customRenderer.paragraph = function renderText(text) {
-  return `<p class='paragraph'>${text}</p>`;
-};
+const getRenderer = (options?: Partial<RendererGetterOption>) => {
+  const renderer = new marked.Renderer();
 
-// 解析标题
-customRenderer.heading = function renderHeading(text, level, raw) {
-  const id = raw.toLowerCase().replace(/[^a-zA-Z0-9\u4E00-\u9FA5]/g, '-');
-  return `<h${level} class='title' id=${id} alt=${id} title=${id}>${text}</h${level}>`;
-};
+  renderer.html = html => (options?.purify ? purifyDomString(html) : html);
 
-// 解析链接
-customRenderer.link = function renderLink(hrefProp: string, title: string, text: string) {
-  const isSelf = hrefProp.includes('http://localhost:3000');
-  let href = hrefProp;
-  if (isSelf) {
-    href = hrefProp.replace('http://localhost:3000', '');
-  }
-  const textIsImage = text.includes('<img');
-  const textIsCode = text.includes('<code');
+  // 解析段落
+  renderer.paragraph = function renderText(text) {
+    return `<p class='paragraph'>${text}</p>`;
+  };
 
-  return `<a
-            href=${href}
-            target='_blank'
-            class="${textIsImage ? 'image-link' : 'link'}"
-            title=${title || (textIsImage || textIsCode ? href : text)}
-            ${isSelf ? '' : "rel='external noopener'"}
-          >
-            ${text}
-          </a>`;
-};
+  // 解析标题
+  renderer.heading = (html, level, raw) => {
+    const idText = options?.headingID
+      ? `id="${options.headingID(html, level, raw)}"`
+      : '';
+    const safeRaw = escape(raw);
+    return `<h${level} ${idText} alt="${safeRaw}" title="${safeRaw}">${html}</h${level}>`;
+  };
 
-// 解析图片
-customRenderer.image = function renderImage(src: string, title: string, text: string) {
-  return `<img
-            name=${`article-cover-${title || text}`}
-            src=${src}
-            title=${title || text}
-            alt=${title || text}
-          />`;
-};
-
-// 解析代码
-customRenderer.code = function renderCode(
-  code: string,
-  language: string,
-  escaped: boolean
-) {
-  if ((this as any).options.highlight) {
-    const output = (this as any).options.highlight(code, language);
-    if (output !== null) {
-      // eslint-disable-next-line no-param-reassign
-      code = output;
-      // eslint-disable-next-line no-param-reassign
-      escaped = true;
+  // 解析链接
+  renderer.link = function renderLink(hrefProp: string, title: string, text: string) {
+    const isSelf = hrefProp.startsWith(WEB_URL);
+    let href = hrefProp;
+    if (isSelf) {
+      href = hrefProp.replace(WEB_URL, '');
     }
-  }
+    const textIsImage = text.includes('<img');
+    const textIsCode = text.includes('<code');
 
-  /**
-   * TODO:
-   * <code>
-   *   {code}
-   * </code>
-   * 为什么这么写会有一个换行
-   */
-  return language
-    ? `<pre data-lang=${language}><code class='${
-        (this as any).options.langPrefix
-      }${escape(language)}'>${escaped ? code : escape(code)}</code></pre>`
-    : `<pre><code>${escaped ? code : escape(code)}\n</code></pre>`;
-};
+    return `<a
+              href=${href}
+              target='_blank'
+              title=${title || (textIsImage || textIsCode ? href : text)}
+              ${isSelf ? '' : "rel='external noopener'"}
+            >
+              ${text}
+            </a>`;
+  };
 
-// 解析文字
-customRenderer.text = function renderText(text: string) {
-  return text;
+  // 解析图片
+  renderer.image = function renderImage(src: string, title: string, text: string) {
+    return `<img
+              name=${`article-cover-${title || text}`}
+              src=${src}
+              title=${title || text}
+              alt=${title || text}
+            />`;
+  };
+
+  // 解析代码
+  renderer.code = function renderCode(code: string, language: string, escaped: boolean) {
+    if ((this as any).options.highlight) {
+      const output = (this as any).options.highlight(code, language);
+      if (output !== null) {
+        // eslint-disable-next-line no-param-reassign
+        code = output;
+        // eslint-disable-next-line no-param-reassign
+        escaped = true;
+      }
+    }
+
+    /**
+     * TODO:
+     * <code>
+     *   {code}
+     * </code>
+     * 为什么这么写会有一个换行
+     */
+    return language
+      ? `<pre data-lang=${language}><code class='${
+          (this as any).options.langPrefix
+        }${escape(language)}'>${escaped ? code : escape(code)}</code></pre>`
+      : `<pre><code>${escaped ? code : escape(code)}\n</code></pre>`;
+  };
+
+  // 解析文字
+  renderer.text = function renderText(text: string) {
+    return text;
+  };
+
+  return renderer;
 };
 
 // 解析参数
 type MarkedOptions = {
   purify?: boolean;
+  headingIDRenderer?: RendererGetterOption['headingID'];
 };
 
-// markdown -> html
-const markedToHtml = (markdown: string, { purify }: MarkedOptions = {}) => {
-  if (!markdown || typeof markdown !== 'string') return '';
+export const markedToHtml = (markdown: string, options?: MarkedOptions) => {
+  if (!markdown || typeof markdown !== 'string') {
+    return '';
+  }
 
-  const purifyMarkdown = purify ? purifyDomString(markdown) : markdown;
+  const renderOptions: Partial<RendererGetterOption> = {
+    headingID: options?.headingIDRenderer,
+  };
 
-  return marked.parse(purifyMarkdown, {
-    renderer: customRenderer,
-  });
+  // sanitize
+  if (options?.purify) {
+    renderOptions.purify = true;
+  }
+
+  return marked.parse(markdown, { renderer: getRenderer(renderOptions) });
 };
 
 export default markedToHtml;

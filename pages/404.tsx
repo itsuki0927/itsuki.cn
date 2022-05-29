559ae4b47313619a82c2db3f525b9831fda1f052
itsuki0927 <2309899048@qq.com>
2022-05-29 22:19:27 +0800

    style: update 404 & remove 500

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,13 +1,28 @@
+import Link from 'next/link';
 import { ReactNode } from 'react';
-import { ErrorHandler, Layout } from '@/components/common';
+import { Layout } from '@/components/common';
 
 const NotFound = () => (
   <div className='flex h-screen items-center justify-center'>
-    <ErrorHandler
-      title='404'
-      message='如果时光倒流， 我们又能抓得住什么 ？'
-      img='/404.jpg'
-    />
+    <div className='flex flex-col items-center justify-center'>
+      <div className='mx-auto flex max-w-2xl flex-col items-start justify-center'>
+        <h1 className='mb-4 text-3xl font-medium tracking-tight md:text-5xl'>
+          404 – Not Found
+        </h1>
+        <div className='mb-8'>
+          <p className='mb-4'>它看起来很神秘，为什么还要展示一个通用的404呢？因为好看.</p>
+          <p className='mb-2'>
+            1. 有可能你访问了一个不存在的资源, 或者是找到以前存在的东西.
+          </p>
+          <p>2. 还有可能是你拼写错了什么, 你能再检查一下那个URL吗？</p>
+        </div>
+        <Link href='/'>
+          <span className='mx-auto w-64 cursor-pointer rounded-sm bg-gray-1 p-1 text-center font-bold opacity-70 transition-opacity hover:opacity-100 dark:text-white sm:p-4'>
+            回到首页
+          </span>
+        </Link>
+      </div>
+    </div>
   </div>
 );
 

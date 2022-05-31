9695b984abff9767ea8162eea2fb5daff387da2b
itsuki0927 <2309899048@qq.com>
2022-05-31 13:09:46 +0800

    fix: fix Link register onClick warning

--- components/article/ArticlePagination/index.tsx
+++ components/article/ArticlePagination/index.tsx
@@ -1,55 +1,59 @@
-import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { Article } from '@/entities/article';
 import { gtag } from '@/utils/gtag';
 import { GAEventCategories } from '@/constants/gtag';
+import { getArticleDetailRoute } from '@/utils/url';
 
 interface ArticlePaginationProps {
   prevArticle: Article | null;
   nextArticle: Article | null;
 }
 
-const ArticlePagination = ({ prevArticle, nextArticle }: ArticlePaginationProps) => (
-  <div className='flex justify-between'>
-    <div className='w-1/2 border-r border-solid border-white-2 bg-white py-4 px-6  '>
-      <span className='mb-2'>上一篇</span>
-      {prevArticle ? (
-        <Link
-          href={`/article/${prevArticle.id}`}
-          onClick={() => {
-            gtag.event('prev_article', {
-              category: GAEventCategories.Article,
-            });
-          }}
-        >
-          <h3 className='cursor-pointer text-sm text-gray-1 transition-colors duration-500 hover:text-dark-2 '>
+const ArticlePagination = ({ prevArticle, nextArticle }: ArticlePaginationProps) => {
+  const router = useRouter();
+  return (
+    <div className='flex justify-between'>
+      <div className='w-1/2 border-r border-solid border-white-2 bg-white py-4 px-6  '>
+        <span className='mb-2'>上一篇</span>
+        {prevArticle ? (
+          <h3
+            key='prev'
+            onClick={() => {
+              router.push(getArticleDetailRoute(prevArticle.id));
+              gtag.event('prev_article', {
+                category: GAEventCategories.Article,
+              });
+            }}
+            className='cursor-pointer text-sm text-gray-1 transition-colors duration-500 hover:text-dark-2 '
+          >
             {prevArticle.title}
           </h3>
-        </Link>
-      ) : (
-        <h3 className='text-sm text-gray-1'>无</h3>
-      )}
-    </div>
+        ) : (
+          <h3 className='text-sm text-gray-1'>无</h3>
+        )}
+      </div>
 
-    <div className='w-1/2 bg-white py-4 px-6 text-right '>
-      <span className='mb-2'>下一篇</span>
-      {nextArticle ? (
-        <Link
-          href={`/article/${nextArticle.id}`}
-          onClick={() => {
-            gtag.event('next_article', {
-              category: GAEventCategories.Article,
-            });
-          }}
-        >
-          <h3 className='cursor-pointer text-right text-sm text-gray-1 transition-colors duration-500 hover:text-dark-2 '>
+      <div className='w-1/2 bg-white py-4 px-6 text-right '>
+        <span className='mb-2'>下一篇</span>
+        {nextArticle ? (
+          <h3
+            key='next'
+            onClick={() => {
+              router.push(getArticleDetailRoute(nextArticle.id));
+              gtag.event('next_article', {
+                category: GAEventCategories.Article,
+              });
+            }}
+            className='cursor-pointer text-right text-sm text-gray-1 transition-colors duration-500 hover:text-dark-2 '
+          >
             {nextArticle.title}
           </h3>
-        </Link>
-      ) : (
-        <h3 className='text-sm text-gray-1 '>无</h3>
-      )}
+        ) : (
+          <h3 className='text-sm text-gray-1 '>无</h3>
+        )}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ArticlePagination;

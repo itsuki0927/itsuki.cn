acf26659bab011754e829b013edfaa042768cae9
itsuki0927 <2309899048@qq.com>
2022-06-08 16:58:04 +0800

    fix: fix getAllTags not found

--- api/tag.ts
+++ api/tag.ts
@@ -1,11 +1,19 @@
 import request from 'graphql-request';
 import { QueryTagResponse } from '@/entities/tag';
 import { endpoint } from './service';
-import { QUERY_TAG } from '@/graphqls/tag';
+import { QUERY_TAGS, QUERY_TAG_PATHS } from '@/graphqls/tag';
 
 export const getAllTagPaths = async () => {
-  const { tags } = await request<QueryTagResponse, any>(endpoint, QUERY_TAG, {
+  const { tags } = await request<QueryTagResponse, any>(endpoint, QUERY_TAG_PATHS, {
     search: {},
   });
   return tags.data.map(item => `/tag/${item.path}`);
+};
+
+export const getAllTags = async () => {
+  const { tags } = await request<QueryTagResponse, any>(endpoint, QUERY_TAGS, {
+    search: {},
+  });
+  console.log('data', tags);
+  return tags.data;
 };

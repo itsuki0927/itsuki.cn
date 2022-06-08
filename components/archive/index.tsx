5bf14e2c81e5d96c2ac53a75affb0dc033ee10d8
itsuki0927 <2309899048@qq.com>
2022-06-08 16:59:47 +0800

    style: update archive style

--- components/archive/index.tsx
+++ components/archive/index.tsx
@@ -3,31 +3,33 @@ import Link from 'next/link';
 import { ArticleArchive, ArticleArchiveResponse } from '@/entities/article';
 
 const getDay = (date: Date) =>
-  `${String(new Date(date).getMonth() + 1).padStart(2, '0')}月${String(
+  `${String(new Date(date).getMonth() + 1).padStart(2, '0')}-${String(
     new Date(date).getDate()
-  ).padStart(2, '0')}号`;
+  ).padStart(2, '0')}`;
 
 const ArticleList = ({ articles }: { articles: ArticleArchive[] }) => (
-  <ul className='relative ml-9 list-none border-l-2 border-dashed border-l-gray-1 pl-8 '>
+  <ul className='relative list-none space-y-8'>
     {articles.map(article => (
-      <div className='mb-8 w-full'>
-        <p className='mb-4 w-32 text-right text-sm text-gray-2 md:mb-0 md:text-left'>
+      <li
+        key={article.id}
+        className='flex items-center rounded-sm p-4 transition-colors hover:bg-gray-100'
+      >
+        <p className='mb-0 w-32 text-sm text-gray-2 sm:text-base'>
           {getDay(article.createAt)}
         </p>
-        <div className='flex flex-col justify-between md:flex-row'>
+        <div className='flex w-full flex-col-reverse justify-between text-sm sm:flex-row sm:text-base'>
           <Link key={article.id} href={`/article/${article.id}`}>
-            <h4 className='mb-2 w-full cursor-pointer text-lg font-medium transition-colors hover:text-dark-2 md:text-xl'>
+            <h4 className='capsize cursor-pointer text-lg font-medium transition-colors hover:text-primary sm:text-xl'>
               {article.title}
             </h4>
           </Link>
-          <p className='mb-4 w-32 text-left text-gray-2 md:mb-0 md:text-right'>
+          <p className='w-32 text-left text-gray-2 md:mb-0 md:text-right'>
             {`${
               article.reading ? Number(article.reading).toLocaleString() : '–––'
             } views`}
           </p>
         </div>
-        <p className='text-gray-3'>{article.description}</p>
-      </div>
+      </li>
     ))}
   </ul>
 );
@@ -37,24 +39,19 @@ interface ArchivePageProps {
 }
 
 const ArchiveView = ({ archives = new Map() }: ArchivePageProps) => (
-  <div className='mx-auto w-3/4 tracking-wider'>
+  <div className='px-4 tracking-wider'>
     <NextSeo title='归档' />
-    <div className='p-6'>
-      <h1 className='mb-4 text-3xl font-medium tracking-tight text-dark-3 md:text-5xl'>
-        归档
-      </h1>
 
-      <ul className='list-none pl-0'>
-        {[...archives.entries()].reverse().map(([year, articles]) => (
-          <li key={year} className='mb-6 rounded-sm'>
-            <h3 className='mt-8 mb-4 text-2xl font-medium tracking-tight text-dark-3 md:text-4xl'>
-              {year}
-            </h3>
-            <ArticleList articles={articles} />
-          </li>
-        ))}
-      </ul>
-    </div>
+    <ul className='list-none pl-0'>
+      {[...archives.entries()].reverse().map(([year, articles]) => (
+        <li key={year} className='mb-6 rounded-sm'>
+          <h3 className='mt-8 mb-4 text-2xl font-medium tracking-tight text-dark-3 md:text-4xl'>
+            {year}
+          </h3>
+          <ArticleList articles={articles} />
+        </li>
+      ))}
+    </ul>
   </div>
 );
 
--- pages/archive.tsx
+++ pages/archive.tsx
@@ -1,11 +1,9 @@
-import { ReactNode } from 'react';
 import { dehydrate, QueryClient } from 'react-query';
 import { getArchives } from '@/api/article';
 import ArchiveView from '@/components/archive';
-import { Layout } from '@/components/common';
-import { articleKeys, globalDataKeys } from '@/constants/queryKeys';
+import { Layout, Navbar } from '@/components/common';
+import { articleKeys } from '@/constants/queryKeys';
 import { useArchives } from '@/hooks/article';
-import { getGlobalData } from '@/api/global';
 import { useMount } from '@/hooks';
 import { gtag } from '@/utils/gtag';
 import { GAEventCategories } from '@/constants/gtag';
@@ -13,7 +11,6 @@ import { GAEventCategories } from '@/constants/gtag';
 export const getStaticProps = async () => {
   const queryClient = new QueryClient();
   await queryClient.prefetchQuery(articleKeys.archive(), () => getArchives());
-  await queryClient.prefetchQuery(globalDataKeys.globalData, () => getGlobalData());
 
   return {
     props: {
@@ -32,9 +29,23 @@ const ArchivePage = () => {
     });
   });
 
-  return <ArchiveView archives={archives.data} />;
+  return (
+    <Layout
+      hero={
+        <div className='space-y-20 bg-white py-10'>
+          <Navbar />
+          <div className='container px-4'>
+            <h1 className='mb-4 text-3xl font-medium tracking-tight text-dark-3 md:text-5xl'>
+              归档
+            </h1>
+            <p>将文章归档</p>
+          </div>
+        </div>
+      }
+    >
+      <ArchiveView archives={archives.data} />
+    </Layout>
+  );
 };
-
-ArchivePage.getLayout = (page: ReactNode) => <Layout showSidebar={false}>{page} </Layout>;
 
 export default ArchivePage;

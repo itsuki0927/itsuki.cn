c25f258c14037c92dbdd922a485dfe86cfe180e8
itsuki0927 <2309899048@qq.com>
2022-06-08 16:54:13 +0800

    feat: add mobile menu

---
+++ components/common/Layout/MobileMenu.tsx
@@ -0,0 +1,152 @@
+import cn from 'classnames';
+import Link from 'next/link';
+import useDelayedRender from 'use-delayed-render';
+import { useState, useEffect } from 'react';
+import styles from './mobile-menu.module.css';
+
+const MenuIcon = (props: JSX.IntrinsicElements['svg']) => (
+  <svg
+    className='absolute h-5 w-5 text-gray-900'
+    width='20'
+    height='20'
+    viewBox='0 0 20 20'
+    fill='none'
+    {...props}
+  >
+    <path
+      d='M2.5 7.5H17.5'
+      stroke='currentColor'
+      strokeWidth='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      d='M2.5 12.5H17.5'
+      stroke='currentColor'
+      strokeWidth='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+  </svg>
+);
+
+const CrossIcon = (props: JSX.IntrinsicElements['svg']) => (
+  <svg
+    className='absolute h-5 w-5 text-gray-900'
+    viewBox='0 0 24 24'
+    width='24'
+    height='24'
+    stroke='currentColor'
+    strokeWidth='1.5'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    fill='none'
+    shapeRendering='geometricPrecision'
+    {...props}
+  >
+    <path d='M18 6L6 18' />
+    <path d='M6 6l12 12' />
+  </svg>
+);
+
+const MobileMenu = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { mounted: isMenuMounted, rendered: isMenuRendered } = useDelayedRender(
+    isMenuOpen,
+    {
+      enterDelay: 20,
+      exitDelay: 300,
+    }
+  );
+
+  function toggleMenu() {
+    if (isMenuOpen) {
+      setIsMenuOpen(false);
+      document.body.style.overflow = '';
+    } else {
+      setIsMenuOpen(true);
+      document.body.style.overflow = 'hidden';
+    }
+  }
+
+  useEffect(
+    () => () => {
+      document.body.style.overflow = '';
+    },
+    []
+  );
+
+  return (
+    <>
+      <button
+        className={cn(styles.burger, 'visible md:hidden')}
+        aria-label='Toggle menu'
+        type='button'
+        onClick={toggleMenu}
+      >
+        <MenuIcon data-hide={isMenuOpen} />
+        <CrossIcon data-hide={!isMenuOpen} />
+      </button>
+      {isMenuMounted && (
+        <ul
+          className={cn(
+            styles.menu,
+            'absolute flex flex-col bg-white',
+            isMenuRendered && styles.menuRendered
+          )}
+        >
+          <li
+            className='border-b border-gray-300 text-sm font-semibold text-gray-900'
+            style={{ transitionDelay: '150ms' }}
+          >
+            <Link href='/'>
+              <a className='flex w-auto pb-4'>首页</a>
+            </Link>
+          </li>
+          <li
+            className='border-b border-gray-300 text-sm font-semibold text-gray-900'
+            style={{ transitionDelay: '175ms' }}
+          >
+            <Link href='/category/code'>
+              <a className='flex w-auto pb-4'>何文</a>
+            </Link>
+          </li>
+          <li
+            className='border-b border-gray-300 text-sm font-semibold text-gray-900'
+            style={{ transitionDelay: '200ms' }}
+          >
+            <Link href='/category/talk'>
+              <a className='flex w-auto pb-4'>何言</a>
+            </Link>
+          </li>
+          <li
+            className='border-b border-gray-300 text-sm font-semibold text-gray-900'
+            style={{ transitionDelay: '250ms' }}
+          >
+            <Link href='/guestbook'>
+              <a className='flex w-auto pb-4'>留言板</a>
+            </Link>
+          </li>
+          <li
+            className='border-b border-gray-300 text-sm font-semibold text-gray-900'
+            style={{ transitionDelay: '275ms' }}
+          >
+            <Link href='/archive'>
+              <a className='flex w-auto pb-4'>归档</a>
+            </Link>
+          </li>
+          <li
+            className='border-b border-gray-300 text-sm font-semibold text-gray-900'
+            style={{ transitionDelay: '300ms' }}
+          >
+            <Link href='/about'>
+              <a className='flex w-auto pb-4'>关于</a>
+            </Link>
+          </li>
+        </ul>
+      )}
+    </>
+  );
+};
+
+export default MobileMenu;

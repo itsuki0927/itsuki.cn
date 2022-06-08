7381dd4e47047780176345286998aa44d02d1552
itsuki0927 <2309899048@qq.com>
2022-06-08 16:49:40 +0800

    chore: clean Navbar

--- components/common/Navbar/index.tsx
+++ components/common/Navbar/index.tsx
@@ -3,18 +3,12 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import classNames from 'classnames';
 import { Logo } from '@/components/common';
-import { Category } from '@/entities/category';
-import { RssIcon } from '@/components/icons';
-import { getCategoryRoute } from '@/utils/url';
-import { shank } from '@/utils/array';
-import ThemeSwitch, { ThemeSwitchProps } from '../ThemeSwitch';
+import { GithubOutlined, RssIcon } from '@/components/icons';
+import ThemeSwitch from '../ThemeSwitch';
 import { gtag } from '@/utils/gtag';
 import { GAEventCategories } from '@/constants/gtag';
-
-type NavbarProps = Omit<ThemeSwitchProps, 'onChange'> & {
-  links?: Category[];
-  onThemeChange: ThemeSwitchProps['onChange'];
-};
+import MobileMenu from '../Layout/MobileMenu';
+import { useTheme } from '@/hooks';
 
 const NavbarItem = ({ href, children }: PropsWithChildren<{ href: string }>) => {
   const { asPath } = useRouter();
@@ -23,7 +17,7 @@ const NavbarItem = ({ href, children }: PropsWithChildren<{ href: string }>) => 
   return (
     <div
       className={classNames(
-        'capsize relative cursor-pointer px-5 text-center tracking-widest transition-colors duration-500 hover:text-primary-hover',
+        'capsize relative hidden cursor-pointer px-5 text-center tracking-widest transition-colors duration-500 hover:text-primary-hover md:inline-block',
         isActive ? 'text-primary' : 'text-dark-2'
       )}
     >
@@ -33,63 +27,83 @@ const NavbarItem = ({ href, children }: PropsWithChildren<{ href: string }>) => 
 };
 
 const DEFAULT_NAV_LIST = [
-  { path: '/', name: '首页' },
+  { path: '/blog', name: '文章' },
   { path: '/guestbook', name: '留言' },
   { path: '/archive', name: '归档' },
   { path: '/about', name: '关于' },
 ];
 
-const Navbar = ({ links, ...themeProps }: NavbarProps) => {
+const Navbar = () => {
+  const [theme, setTheme] = useTheme();
   const categoriesDom = useMemo(() => {
-    const addNavList =
-      links?.map(item => ({ ...item, path: getCategoryRoute(item.path) })) || [];
-
     // 将查询到的分类添加到首页的后面
-    const navDom = shank(DEFAULT_NAV_LIST, 1, 0, ...addNavList).map(item => (
+    const navDom = DEFAULT_NAV_LIST.map(item => (
       <NavbarItem key={item.path} href={item.path}>
         {item.name}
       </NavbarItem>
     ));
 
     // 添加主题按钮
-    return navDom.concat(
-      <div
-        tabIndex={0}
-        role='button'
-        key='theme'
-        className='px-5 text-center'
-        onClick={() => {
-          gtag.event('theme_switch', {
-            category: GAEventCategories.Widget,
-            label: themeProps.theme,
-          });
-        }}
-      >
-        <ThemeSwitch theme={themeProps.theme} onChange={themeProps.onThemeChange} />
-      </div>,
-      <div
-        tabIndex={0}
-        role='button'
-        key='rss'
-        className='px-5 text-center'
-        onClick={() => {
-          gtag.event('rss', {
-            category: GAEventCategories.Widget,
-            label: themeProps.theme,
-          });
-        }}
-      >
-        <RssIcon />
-      </div>
-    );
-  }, [links, themeProps]);
+    return navDom;
+  }, []);
 
   return (
-    <header className='fixed inset-x-0 z-10 h-20 bg-[#ffffff80] backdrop-blur-[2px] backdrop-saturate-150 dark:bg-[#0d0d1050]'>
-      <div className='container flex h-full items-center justify-between'>
-        <Logo />
-        <nav className='flex h-full items-center'>{categoriesDom}</nav>
-      </div>
+    <header className='inset-x-0 z-10'>
+      <nav className='container flex h-full items-center justify-between px-4'>
+        <div className='relative flex flex-grow items-center justify-between md:justify-start'>
+          <Logo />
+          <div className='ml-5'>
+            {categoriesDom}
+            <MobileMenu />
+          </div>
+        </div>
+
+        <div>
+          <div
+            tabIndex={0}
+            role='button'
+            key='theme'
+            className='hidden px-5 text-center md:inline-block'
+            onClick={() => {
+              gtag.event('theme_switch', {
+                category: GAEventCategories.Widget,
+                label: theme,
+              });
+            }}
+          >
+            <ThemeSwitch theme={theme} onChange={setTheme} />
+          </div>
+          <div
+            tabIndex={0}
+            role='button'
+            key='rss'
+            className='hidden px-5 text-center md:inline-block'
+            onClick={() => {
+              gtag.event('rss', {
+                category: GAEventCategories.Widget,
+                label: 'rss',
+              });
+            }}
+          >
+            <RssIcon />
+          </div>
+
+          <div
+            tabIndex={0}
+            role='button'
+            key='github'
+            className='hidden px-5 text-center md:inline-block'
+            onClick={() => {
+              gtag.event('github', {
+                category: GAEventCategories.Widget,
+                label: 'github',
+              });
+            }}
+          >
+            <GithubOutlined className='text-xl' />
+          </div>
+        </div>
+      </nav>
     </header>
   );
 };

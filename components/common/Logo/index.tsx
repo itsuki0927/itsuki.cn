1e657a01f2d4ba12a0e6b1e97e204145acb48262
itsuki0927 <2309899048@qq.com>
2022-06-08 16:47:15 +0800

    chore: clean logo

--- components/common/Logo/index.tsx
+++ components/common/Logo/index.tsx
@@ -1,26 +1,23 @@
+import Link from 'next/link';
 import { LogoJsonLd } from 'next-seo';
-import router from 'next/router';
 import { RESOURCE_URL, WEB_URL } from '@/configs/app';
 import { MyImage } from '..';
 
 const Logo = () => (
-  <div
-    className='cursor-pointer overflow-hidden'
-    role='img'
-    aria-hidden='true'
-    onClick={() => router.push('/')}
-  >
-    <MyImage
-      src='/logo.png'
-      objectFit='cover'
-      width={60}
-      height={60}
-      unoptimized
-      alt='logo'
-      className='h-full'
-    />
-    <LogoJsonLd logo={`${RESOURCE_URL}/logo.png`} url={WEB_URL} />
-  </div>
+  <Link href='/'>
+    <>
+      <MyImage
+        src='/logo.png'
+        objectFit='cover'
+        width={60}
+        height={60}
+        unoptimized
+        alt='logo'
+        className='h-full'
+      />
+      <LogoJsonLd logo={`${RESOURCE_URL}/logo.png`} url={WEB_URL} />
+    </>
+  </Link>
 );
 
 export default Logo;

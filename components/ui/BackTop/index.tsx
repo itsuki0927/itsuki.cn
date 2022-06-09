cd42efadbbb8a76214ec81342178239c77d7a8e6
itsuki0927 <2309899048@qq.com>
2022-06-09 21:37:37 +0800

    style: fix BackTop mobile position

--- components/ui/BackTop/index.tsx
+++ components/ui/BackTop/index.tsx
@@ -5,7 +5,7 @@ const BackTop = () => {
   const backToTop = () => scrollTo(0, 600);
 
   return (
-    <div className='fixed bottom-36 right-6 z-10'>
+    <div className='fixed bottom-9 right-4 z-10 sm:bottom-36'>
       <button
         type='button'
         className='rounded-sm bg-primary px-3 py-1 text-white opacity-40 transition-opacity duration-300 hover:opacity-100'

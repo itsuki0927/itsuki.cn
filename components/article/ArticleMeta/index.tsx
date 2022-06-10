b112143caf50778d1c36bf14824b15ac30df0905
itsuki0927 <2309899048@qq.com>
2022-06-10 14:15:22 +0800

    chore: update ArticleMeta color & use ExternalLink replace a link

--- components/article/ArticleMeta/index.tsx
+++ components/article/ArticleMeta/index.tsx
@@ -1,10 +1,11 @@
 import { useRouter } from 'next/router';
 import { WEB_URL } from '@/configs/app';
+import { ExternalLink } from '@/components/common';
 
 const ArticleMeta = () => {
   const router = useRouter();
   return (
-    <div className='my-4 flex flex-col text-sm text-gray-1 sm:flex-row sm:items-center sm:justify-center'>
+    <div className='my-4 flex flex-col text-sm text-gray-2 sm:flex-row sm:items-center sm:justify-center'>
       <div>
         <span>永久地址: </span>
         <span className='underline'>{WEB_URL + router.asPath}</span>
@@ -12,14 +13,12 @@ const ArticleMeta = () => {
 
       <div className='mt-2 sm:ml-2 sm:mt-0'>
         <span>版权声明: </span>
-        <a
-          className='hover: no-underline transition-colors hover:text-basic'
+        <ExternalLink
+          className='no-underline transition-colors hover:text-basic hover:underline'
           href='https://creativecommons.org/licenses/by-nc/3.0/cn/deed.zh'
-          target='_blank'
-          rel='external nofollow noopener noreferrer'
         >
           自由转载 - 署名 - 非商业使用
-        </a>
+        </ExternalLink>
       </div>
     </div>
   );

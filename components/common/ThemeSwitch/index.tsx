9052b6fa5315030a7efcd05ee4af514501965183
itsuki0927 <2309899048@qq.com>
2022-06-10 10:08:32 +0800

    chore: remove react-toggle-dark-mode

--- components/common/ThemeSwitch/index.tsx
+++ components/common/ThemeSwitch/index.tsx
@@ -1,35 +1,47 @@
-import dynamic from 'next/dynamic';
 import Head from 'next/head';
-import { DarkModeSwitch as DarkModeSwitchFacker } from 'react-toggle-dark-mode';
-import { ColorTheme } from '@/hooks/useTheme';
+import useTheme from '@/hooks/useTheme';
 
-const DarkModeSwitch = dynamic(
-  () =>
-    import('react-toggle-dark-mode').then(mod => mod.DarkModeSwitch) as Promise<
-      typeof DarkModeSwitchFacker
-    >,
-  { loading: () => <div className='h-5 w-5' /> }
-);
+const ThemeSwitch = () => {
+  const [theme, setTheme] = useTheme();
+  return (
+    <>
+      <Head>
+        <meta name='theme-color' content={theme === 'dark' ? '#000' : '#fdfdfd'} />
+      </Head>
 
-export interface ThemeSwitchProps {
-  theme: ColorTheme;
-  onChange: (checked: boolean) => void;
-}
-
-const ThemeSwitch = ({ theme, onChange }: ThemeSwitchProps) => (
-  <>
-    <Head>
-      <meta name='theme-color' content={theme === 'dark' ? '#000' : '#fdfdfd'} />
-    </Head>
-    <div className='flex h-5 w-5 items-center bg-transparent'>
-      <DarkModeSwitch
-        checked={theme === 'dark'}
-        onChange={onChange}
-        moonColor='rgb(168 160 149)'
-        sunColor='#2d2d2d'
-      />
-    </div>
-  </>
-);
+      <button
+        aria-label='Toggle Dark Mode'
+        type='button'
+        className='flex h-6 w-6 items-center justify-center rounded-lg transition-all'
+        onClick={setTheme}
+      >
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          viewBox='0 0 24 24'
+          fill='none'
+          stroke='currentColor'
+          width={24}
+          height={24}
+        >
+          {theme === 'dark' ? (
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth={2}
+              d='M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z'
+            />
+          ) : (
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth={2}
+              d='M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z'
+            />
+          )}
+        </svg>
+      </button>
+    </>
+  );
+};
 
 export default ThemeSwitch;

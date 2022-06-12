9dab5c8f799e708472025db5e91e0b4a759f5e0d
itsuki0927 <2309899048@qq.com>
2022-06-12 08:33:28 +0800

    chore: add ImagePopup src has value display

--- components/ui/Popup/ImagePopup.tsx
+++ components/ui/Popup/ImagePopup.tsx
@@ -2,13 +2,14 @@ export type PopupProps = {
   src?: string;
 };
 
-const ImagePopup = ({ src }: PopupProps) => (
-  <img
-    id='popupImage'
-    alt='popup show'
-    src={src}
-    className='max-h-[80%] max-w-screen-md rounded-md border-8 border-solid border-white-3 object-cover '
-  />
-);
+const ImagePopup = ({ src }: PopupProps) =>
+  src ? (
+    <img
+      id='popupImage'
+      alt='popup show'
+      src={src}
+      className='max-h-[80%] max-w-screen-md rounded-md border-8 border-solid border-white-3 object-cover '
+    />
+  ) : null;
 
 export default ImagePopup;

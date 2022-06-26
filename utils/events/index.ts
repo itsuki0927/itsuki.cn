9c193f0f482958cbc4c9682d0d0b85c6af08af72
itsuki0927 <2309899048@qq.com>
2022-06-27 07:06:05 +0800

    chore: on、off migrated to events/index.ts

--- utils/events/index.ts
+++ utils/events/index.ts
@@ -1,2 +1,45 @@
-export { default as on } from './on';
-export { default as off } from './off';
+/**
+ * 给元素节点移除事件
+ *
+ * @param target 元素节点
+ * @param eventType 事件名
+ * @param listener 事件处理函数
+ * @param options 其他参数
+ * @returns
+ */
+export function off<K extends keyof DocumentEventMap>(
+  target: Document | Element | EventTarget | Window,
+  eventType: K,
+  listener: EventListenerOrEventListenerObject | CustomEventListener,
+  options: boolean | AddEventListenerOptions = false
+) {
+  target.removeEventListener(eventType, listener, options);
+}
+
+export interface CustomEventListener<T = any> {
+  (evt: T): void;
+}
+
+/**
+ * 给元素节点绑定事件
+ *
+ * @param target 元素节点
+ * @param eventType 事件名
+ * @param listener 事件处理函数
+ * @param options 其他参数
+ * @returns {off: () => void}
+ */
+export function on<K extends keyof DocumentEventMap>(
+  target: Element | Window | Document | EventTarget,
+  eventType: K,
+  listener: EventListenerOrEventListenerObject | CustomEventListener,
+  options: boolean | AddEventListenerOptions = false
+) {
+  target.addEventListener(eventType, listener, options);
+
+  return {
+    off() {
+      target.removeEventListener(eventType, listener, options);
+    },
+  };
+}

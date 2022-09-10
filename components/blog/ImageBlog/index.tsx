108397636d674245a331ce2392e2007431f080e0
itsuki0927 <2309899048@qq.com>
2022-09-10 23:59:52 +0800

    feat: add image blog content

---
+++ components/blog/ImageBlog/index.tsx
@@ -0,0 +1,43 @@
+import classNames from 'classnames';
+import Link from 'next/link';
+import { MyImage, ToDate } from '@/components/common';
+import { Article } from '@/entities/article';
+import { getBlogDetailRoute } from '@/utils/url';
+
+interface ImageBlogProps {
+  blog?: Article;
+}
+
+const ImageBlog = ({ blog }: ImageBlogProps) => {
+  if (!blog) return <div>null</div>;
+
+  return (
+    <article
+      className={classNames(
+        'relative aspect-square w-full justify-center p-6 sm:w-1/3 sm:max-w-sm'
+      )}
+    >
+      <MyImage
+        layout='fill'
+        objectFit='cover'
+        src={blog?.cover ?? ''}
+        alt={blog.title}
+        className='absolute top-0 left-0 right-0 bottom-0'
+      />
+
+      <Link href={getBlogDetailRoute(blog.path)}>
+        <h3 className='absolute top-0 left-0 right-0 mt-0 cursor-pointer p-4 text-2xl font-semibold text-white transition-colors hover:text-primary'>
+          {blog.title}
+        </h3>
+      </Link>
+
+      <div className='absolute right-6 bottom-6 bg-gray-800 px-2 text-sm text-gray-300'>
+        <ToDate date={blog.createAt} />
+        <span className='mx-2'>/</span>
+        {blog.author}
+      </div>
+    </article>
+  );
+};
+
+export default ImageBlog;

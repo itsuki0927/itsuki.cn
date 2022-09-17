55673984fd46f458316f402e0febf27b07f0de99
itsuki0927 <2309899048@qq.com>
2022-09-17 10:07:51 +0800

    feat: add common types

---
+++ types/common.ts
@@ -0,0 +1,12 @@
+import { ReactNode, CSSProperties, ElementType } from 'react';
+
+export interface StandardProps {
+  className?: string;
+  children?: ReactNode;
+  style?: CSSProperties;
+}
+
+export interface WithAsProps<As extends ElementType | string = ElementType>
+  extends StandardProps {
+  as?: As;
+}

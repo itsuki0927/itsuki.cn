ab247823b929e6d7f61d7938b41fd088e9d0e94a
itsuki0927 <2309899048@qq.com>
2022-09-17 10:08:55 +0800

    feat: add PtnContainer Component

---
+++ components/ui/PtnContainer/index.tsx
@@ -0,0 +1,15 @@
+import classNames from 'classnames';
+import { WithAsProps } from '@/types/common';
+import styles from './index.module.scss';
+
+export type PtnContainerProps = WithAsProps;
+
+const PtnContainer = ({
+  className = '',
+  as: Component = 'div',
+  ...rest
+}: PtnContainerProps) => (
+  <Component className={classNames(styles.ptn, className)} {...rest} />
+);
+
+export default PtnContainer;

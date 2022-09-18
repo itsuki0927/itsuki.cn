f95e3c0d3592c93175e96c19aa8753351e914590
itsuki0927 <2309899048@qq.com>
2022-09-18 16:49:23 +0800

    chore: navbar add sticky position

--- components/common/Navbar/index.tsx
+++ components/common/Navbar/index.tsx
@@ -74,7 +74,7 @@ interface HeaderProps {
 const Header = ({ theme = 'white' }: HeaderProps) => (
   <header
     className={classNames(
-      'relative inset-x-0 z-50 h-16 backdrop-blur-[20px] backdrop-saturate-150 ',
+      'sticky inset-x-0 top-0 z-50 h-16 backdrop-blur-[20px] backdrop-saturate-150 ',
       theme === 'white' ? 'bg-white' : 'bg-gray-50 '
     )}
   >

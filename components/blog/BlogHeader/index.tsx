06e547876c768461c7bdd2c2af638670f692f1c1
itsuki0927 <2309899048@qq.com>
2022-10-29 10:00:40 +0800

    chore: remove blog detail header tag

--- components/blog/BlogHeader/index.tsx
+++ components/blog/BlogHeader/index.tsx
@@ -8,19 +8,17 @@ interface BlogHeaderProps {
 const BlogHeader = ({ blog }: BlogHeaderProps) => (
   <div className='bg-gray-50 py-24'>
     <Container>
-      <header>
-        <div className='mb-4 inline-block space-x-2 rounded-md bg-primary-light p-1 px-2'>
-          <span className='rounded-sm bg-white py-1 px-2 text-sm text-primary'>
-            {blog.reading} 人阅读
-          </span>
+      <div className='mb-4 inline-block space-x-2 rounded-md bg-primary-light p-1 px-2'>
+        <span className='rounded-sm bg-white py-1 px-2 text-sm text-primary'>
+          {blog.reading} 人阅读
+        </span>
 
-          <span className='rounded-sm py-1 text-sm text-primary'>8 分钟阅读</span>
-        </div>
+        <span className='rounded-sm py-1 text-sm text-primary'>8 分钟阅读</span>
+      </div>
 
-        <h1 className='max-w-3xl text-5xl font-medium'>{blog.title}</h1>
+      <h1 className='max-w-3xl text-5xl font-medium'>{blog.title}</h1>
 
-        <p className='mt-6 max-w-3xl text-xl'>{blog.description}</p>
-      </header>
+      <p className='mt-6 max-w-3xl text-xl'>{blog.description}</p>
     </Container>
   </div>
 );

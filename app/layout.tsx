cf1ae9d5b25a69ed2cb3e43b19b414e1e935b5a6
liusenlin <liusenlin.0927@bytedance.com>
2023-12-31 14:54:19 +0800

    feat: add guestbook entry & vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import metadataConfig from "@/constants/seo";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { IBM_Plex_Sans, Fira_Code } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "react-notion-x/src/styles.css";
 
 export const metadata = metadataConfig;
@@ -30,6 +31,7 @@ export default function RootLayout({
         <Navbar />
         <main className="sm:px-8 my-16 sm:my-24">{children}</main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );

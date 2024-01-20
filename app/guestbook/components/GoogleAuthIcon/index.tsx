9323101b01bc8bbcfc11d2411b4f9a094438416e
liusenlin <liusenlin.0927@bytedance.com>
2024-01-20 22:30:02 +0800

    chore: improve login style

--- app/guestbook/components/GoogleAuthIcon/index.tsx
+++ app/guestbook/components/GoogleAuthIcon/index.tsx
@@ -2,24 +2,26 @@
 
 import type { MouseEvent } from "react";
 import { signIn } from "next-auth/react";
+import { Chrome } from "lucide-react";
 
 interface GoogleIconProps {
-  className?: string;
   onClick?: (e: MouseEvent) => void;
 }
 
-const GoogleAuthIcon = ({ className = "", onClick }: GoogleIconProps) => {
+const GoogleAuthIcon = ({ onClick }: GoogleIconProps) => {
   const handleSignIn = (e: MouseEvent) => {
     signIn();
     onClick?.(e);
   };
 
   return (
-    <button className={className} onClick={handleSignIn} type="button">
-      <span className="inline-flex items-center rounded-lg border border-solid border-gray-200 bg-white px-4 py-2 text-sm text-gray-900 transition-colors hover:bg-gray-100">
-        {/* <GoogleOutlined className="mr-1" /> */}
-        <span className="capsize">Google</span>
-      </span>
+    <button
+      className="inline-flex items-center rounded-lg border border-solid border-gray-200 bg-white py-2 px-4 text-sm text-gray-900 transition-colors hover:bg-gray-100"
+      onClick={handleSignIn}
+      type="button"
+    >
+      <Chrome size={14} className="mr-1" />
+      <span>Google</span>
     </button>
   );
 };

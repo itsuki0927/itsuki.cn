cbdcbe827ea068f3f083cec0bdb7633059ee7b24
liusenlin <liusenlin.0927@bytedance.com>
2024-01-21 22:44:59 +0800

    feat: add not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import Link from 'next/link';
+
+const NotFoundPage = () => {
+  return (
+    <div className="pointer-events-none inset-0 flex py-24 w-full flex-col items-center justify-center">
+      <motion.div
+        className="text-9xl font-extrabold ..."
+        initial={{ opacity: 0, y: 30 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.3, delay: 0.3 }}
+      >
+        <span className="bg-clip-text text-transparent bg-gradient-to-r from-primary to-violet-500 pointer-events-none select-none">
+          404
+        </span>
+      </motion.div>
+      <Link
+        href="/"
+        className="mt-6 pointer-events-auto select-none text-xl font-bold text-white mix-blend-difference hover:underline"
+      >
+        返回主页
+      </Link>
+    </div>
+  );
+};
+
+export default NotFoundPage;

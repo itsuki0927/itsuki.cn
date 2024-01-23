066523658923d33c08c9cf18d024d70c521a7a29
liusenlin <liusenlin.0927@bytedance.com>
2024-01-23 22:14:23 +0800

    feat: add cache

--- libs/notion/getAllBlogs.ts
+++ libs/notion/getAllBlogs.ts
@@ -1,4 +1,5 @@
 import { idToUuid } from 'notion-utils';
+import { unstable_cache as cache } from 'next/cache';
 import getAllPageIds from './getAllPageIds';
 import getPageProperties from './getPageProperties';
 import filterPublishedPosts from './filterPublishedBlogs';
@@ -10,47 +11,53 @@ export interface GetAllBlogsParams {
   onlyRecent?: boolean;
 }
 
-async function getAllBlogs({ onlyRecent }: GetAllBlogsParams = {}) {
-  const { block, schema, collectionQuery } = await getRootPage();
+const getAllBlogs = cache(
+  async ({ onlyRecent }: GetAllBlogsParams = {}) => {
+    const { block, schema, collectionQuery } = await getRootPage();
 
-  const id = idToUuid(NOTION_PAGE_ID);
+    const id = idToUuid(NOTION_PAGE_ID);
 
-  const rawMetadata = block[id]?.value;
+    const rawMetadata = block[id]?.value;
 
-  if (
-    rawMetadata?.type !== 'collection_view_page' &&
-    rawMetadata?.type !== 'collection_view'
-  ) {
-    console.log(`pageId '${id}' is not a database`);
-    return [];
-  } else {
-    // Construct Data
-    const pageIds = getAllPageIds(collectionQuery);
+    if (
+      rawMetadata?.type !== 'collection_view_page' &&
+      rawMetadata?.type !== 'collection_view'
+    ) {
+      console.log(`pageId '${id}' is not a database`);
+      return [];
+    } else {
+      // Construct Data
+      const pageIds = getAllPageIds(collectionQuery);
 
-    const data = pageIds.map((pageId) => {
-      const id = String(pageId);
-      const properties = getPageProperties(id, block, schema) || null;
+      const data = pageIds.map((pageId) => {
+        const id = String(pageId);
+        const properties = getPageProperties(id, block, schema) || null;
 
-      return properties as Blog;
-    });
+        return properties as Blog;
+      });
 
-    const posts = filterPublishedPosts({
-      posts: data,
-      onlyRecent,
-    });
+      const posts = filterPublishedPosts({
+        posts: data,
+        onlyRecent,
+      });
 
-    posts.sort((a, b) => {
-      if (a.publishedAt && b.publishedAt) {
-        return b.publishedAt.valueOf() - a.publishedAt.valueOf();
-      }
-      if (a.createdAt && b.createdAt) {
-        return b.createdAt.valueOf() - a.createdAt.valueOf();
-      }
-      return -1;
-    });
+      posts.sort((a, b) => {
+        if (a.publishedAt && b.publishedAt) {
+          return b.publishedAt.valueOf() - a.publishedAt.valueOf();
+        }
+        if (a.createdAt && b.createdAt) {
+          return b.createdAt.valueOf() - a.createdAt.valueOf();
+        }
+        return -1;
+      });
 
-    return posts;
-  }
-}
+      return posts;
+    }
+  },
+  ['getAllBlogs'],
+  {
+    revalidate: 3600,
+  },
+);
 
 export default getAllBlogs;
--- libs/notion/getRootPage.ts
+++ libs/notion/getRootPage.ts
@@ -1,16 +1,21 @@
+import { unstable_cache as cache } from 'next/cache';
 import { NOTION_PAGE_ID } from '@/constants/env';
 import index from '.';
 
-const getRootPage = async () => {
-  let id = NOTION_PAGE_ID;
-  const response = await index.getPage(id);
+const getRootPage = cache(
+  async () => {
+    let id = NOTION_PAGE_ID;
+    const response = await index.getPage(id);
 
-  const collection = Object.values(response.collection)[0]?.value;
-  const collectionQuery = response.collection_query;
-  const block = response.block;
-  const schema = collection?.schema;
+    const collection = Object.values(response.collection)[0]?.value;
+    const collectionQuery = response.collection_query;
+    const block = response.block;
+    const schema = collection?.schema;
 
-  return { block, schema, collectionQuery };
-};
+    return { block, schema, collectionQuery };
+  },
+  ['getRootPage'],
+  { revalidate: 3600 },
+);
 
 export default getRootPage;

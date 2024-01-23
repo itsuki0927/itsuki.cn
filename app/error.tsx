e6c4fd593116fe692c54181f49d7ad1cccc1a6d4
liusenlin <liusenlin.0927@bytedance.com>
2024-01-23 08:43:49 +0800

    chore: update blocked、404、error page style

--- app/error.tsx
+++ app/error.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { ADMIN_EMAIL1 } from '@/constants/env';
+import { motion } from 'framer-motion';
 import { useEffect } from 'react';
 
 interface ErrorProps {
@@ -15,17 +16,24 @@ const Error = ({ error, reset }: ErrorProps) => {
   }, [error]);
 
   return (
-    <main className="flex py-24 w-full flex-col items-center justify-center text-zinc-800 dark:bg-zinc-900 dark:text-zinc-200">
-      <h1 className="mb-4 text-4xl font-black tracking-tighter">
-        哎呀，不知道为啥出错啦！！！
-      </h1>
-      <span className="text-sm">
+    <section className="pointer-events-none inset-0 flex py-24 w-full flex-col items-center justify-center">
+      <motion.div
+        className="text-4xl font-extrabold"
+        initial={{ opacity: 0, y: 30 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.3, delay: 0.3 }}
+      >
+        <span className="text-gradient-primary pointer-events-none select-none">
+          哎呀，不知道为啥出错啦！！！
+        </span>
+      </motion.div>
+      <span className="mt-6 pointer-events-auto select-none text-sm font-bold text-white mix-blend-difference hover:underline">
         或许你可以刷新一下页面，如果还有问题的话可以联系作者：
         <a href={`mailto:${ADMIN_EMAIL1}`} className="font-bold underline">
           {ADMIN_EMAIL1}
         </a>
       </span>
-    </main>
+    </section>
   );
 };
 

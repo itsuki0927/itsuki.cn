235b1d2ae24446a3bb2e037836dacb0bd3205faa
itsuki0927 <2309899048@qq.com>
2024-02-20 23:11:28 +0800

    feat: add tinybird

---
+++ layouts/AppLayout/components/TinybirdScript.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import Script from "next/script";
+
+const TinybirdScript = () => {
+  return (
+    <Script
+      strategy="lazyOnload"
+      src="https://unpkg.com/@tinybirdco/flock.js"
+      data-host="https://api.tinybird.co"
+      data-token="p.eyJ1IjogIjZhNTRjM2NmLTlhMDYtNGE0Mi04ODQzLWIyYmMxOGVkZmVlOSIsICJpZCI6ICIzOWUwOTg3OS03NGI3LTRjNjYtOWYxZi0yOGY1ZmE2N2U1ODkiLCAiaG9zdCI6ICJldV9zaGFyZWQifQ._M_tCmySZ4raos65WLsYjJm3C879zHqagasGQQhjgVc"
+      onLoad={() => {
+        console.log("Tinybird script loaded");
+      }}
+      onError={(e: Error) => {
+        console.error("Tinybird script failed to load", e);
+      }}
+    />
+  );
+};
+
+export default TinybirdScript;

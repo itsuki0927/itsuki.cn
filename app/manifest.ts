2fca5aec6129354634330a7c1606850ac5908a81
itsuki0927 <2309899048@qq.com>
2024-02-20 23:11:29 +0800

    feat: add sitemap & manifest

---
+++ app/manifest.ts
@@ -0,0 +1,23 @@
+import { META } from '@/constants/app';
+import { MetadataRoute } from 'next';
+
+const manifest = (): MetadataRoute.Manifest => {
+  return {
+    name: META.title,
+    short_name: META.title,
+    description: META.description,
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#fff',
+    theme_color: '#fff',
+    icons: [
+      {
+        src: '/favicon.ico',
+        sizes: 'any',
+        type: 'image/x-icon',
+      },
+    ],
+  };
+};
+
+export default manifest;

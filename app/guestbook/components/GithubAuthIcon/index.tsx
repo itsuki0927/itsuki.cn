ef38e00dc1293a34c07c1f1282310671fb417200
itsuki0927 <2309899048@qq.com>
2024-02-24 15:02:06 +0800

    chore: add github、google className & remove updateSession middleware

--- app/guestbook/components/GithubAuthIcon/index.tsx
+++ app/guestbook/components/GithubAuthIcon/index.tsx
@@ -3,12 +3,14 @@
 import type { MouseEvent } from 'react';
 import { Github } from 'lucide-react';
 import { supabaseBrowserClient } from '@/libs/supabase/client';
+import clsx from 'clsx';
 
 interface GithubIconProps {
   onClick?: (e: MouseEvent) => void;
+  className?: string;
 }
 
-const GithubAuthIcon = ({ onClick }: GithubIconProps) => {
+const GithubAuthIcon = ({ onClick, className }: GithubIconProps) => {
   const handleSignIn = (e: MouseEvent) => {
     onClick?.(e);
     supabaseBrowserClient.auth.signInWithOAuth({
@@ -21,12 +23,15 @@ const GithubAuthIcon = ({ onClick }: GithubIconProps) => {
 
   return (
     <button
-      className="inline-flex items-center  rounded-lg border border-solid border-github bg-github py-2 px-4 text-sm text-white bg-zinc-800 opacity-90 transition-opacity hover:opacity-100"
+      className={clsx(
+        'inline-flex items-center rounded-lg border border-solid border-github bg-github py-2 px-4 text-sm text-white bg-zinc-800 opacity-90 transition-opacity hover:opacity-100',
+        className,
+      )}
       onClick={handleSignIn}
       type="button"
     >
-      <Github size={14} className="mr-1" />
-      <span>Github</span>
+      <Github size={14} className="mr-2" />
+      <span className="text-xs">使用 Github 登陆</span>
     </button>
   );
 };
--- app/guestbook/components/GoogleAuthIcon/index.tsx
+++ app/guestbook/components/GoogleAuthIcon/index.tsx
@@ -3,12 +3,14 @@
 import type { MouseEvent } from 'react';
 import { Chrome } from 'lucide-react';
 import { supabaseBrowserClient } from '@/libs/supabase/client';
+import clsx from 'clsx';
 
 interface GoogleIconProps {
   onClick?: (e: MouseEvent) => void;
+  className?: string;
 }
 
-const GoogleAuthIcon = ({ onClick }: GoogleIconProps) => {
+const GoogleAuthIcon = ({ onClick, className }: GoogleIconProps) => {
   const handleSignIn = (e: MouseEvent) => {
     onClick?.(e);
     supabaseBrowserClient.auth.signInWithOAuth({
@@ -21,12 +23,15 @@ const GoogleAuthIcon = ({ onClick }: GoogleIconProps) => {
 
   return (
     <button
-      className="inline-flex items-center rounded-lg border border-solid border-gray-200 bg-white py-2 px-4 text-sm text-gray-900 transition-colors hover:bg-gray-100"
+      className={clsx(
+        'inline-flex items-center rounded-lg border border-solid border-gray-200 bg-white py-2 px-4 text-sm text-gray-900 transition-colors hover:bg-gray-100',
+        className,
+      )}
       onClick={handleSignIn}
       type="button"
     >
-      <Chrome size={14} className="mr-1" />
-      <span>Google</span>
+      <Chrome size={14} className="mr-2" />
+      <span className="text-xs">使用 Google 登陆</span>
     </button>
   );
 };

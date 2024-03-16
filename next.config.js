c6627a559f2582da73517592bd7326b56ad14186
itsuki0927 <2309899048@qq.com>
2024-03-16 13:25:25 +0800

    fix: next image domain

--- next.config.js
+++ next.config.js
@@ -102,6 +102,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'camo.githubusercontent.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'skfztuyhbelqtomwriol.supabase.co',
+      },
     ],
   },
 };
